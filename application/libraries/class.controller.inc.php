ef6120c4a194ab7cdc92fdd6258016ad0a9ac7f6
Heinz Wiesinger <heinz@m2mobi.com>
2010-09-01 17:11:04 +0200

    application: Add documentation to Controller and Model classes

--- application/libraries/class.controller.inc.php
+++ application/libraries/class.controller.inc.php
@@ -1,23 +1,42 @@
 <?php
 
+/**
+ * Base Controller class
+ * @author M2Mobi, Heinz Wiesinger
+ */
 abstract class Controller
 {
 
+    /**
+     * Constructor
+     */
     public function __construct()
     {
 
     }
 
+    /**
+     * Destructor
+     */
     public function __destruct()
     {
 
     }
 
+    /**
+     * Handle unimplemented calls
+     * @param String $name Method name
+     * @param array $arguments Arguments passed to the method
+     * @return String JSON encoded error String
+     */
     public function __call($name, $arguments)
     {
         return Json::error("not_implemented");
     }
 
+    /**
+     * Default method as defined in conf.application.inc.php
+     */
     abstract public function index();
 
 }
--- application/libraries/class.model.inc.php
+++ application/libraries/class.model.inc.php
@@ -1,13 +1,23 @@
 <?php
 
+/**
+ * Base Model class
+ * @author M2Mobi, Heinz Wiesinger
+ */
 abstract class Model
 {
 
+    /**
+     * Constructor
+     */
     public function __construct()
     {
 
     }
 
+    /**
+     * Destructor
+     */
     public function __destruct()
     {
 
