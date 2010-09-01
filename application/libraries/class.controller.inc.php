a3934f366903ed788a0ecf0778cd7c43528a8a15
Heinz Wiesinger <heinz@m2mobi.com>
2010-09-01 15:59:03 +0200

    application: Controller fixes.
    -) Add an abstract definition for the default method (index)
    -) Add a fallback __call method for undefined calls

--- application/libraries/class.controller.inc.php
+++ application/libraries/class.controller.inc.php
@@ -13,6 +13,13 @@ abstract class Controller
 
     }
 
+    public function __call($name, $arguments)
+    {
+        return Json::error("not_implemented");
+    }
+
+    abstract public function index();
+
 }
 
 ?>
