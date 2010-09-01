915966179e71ecd47e12e83aaab6fe677c0d93d9
Heinz Wiesinger <heinz@m2mobi.com>
2010-09-01 17:10:33 +0200

    application: First version of a standardized View implementation.

--- application/libraries/class.view.inc.php
+++ application/libraries/class.view.inc.php
@@ -1,16 +1,56 @@
 <?php
 
+/**
+ * View class used by the Website
+ * @author M2Mobi, Heinz Wiesinger
+ */
 abstract class View
 {
 
-    public function __construct()
-    {
+    /**
+     * Shared data variable
+     * @var array
+     */
+    protected $data;
 
+    /**
+     * Reference to the Controller who instantiated the View
+     * @var Object
+     */
+    protected $controller;
+
+    /**
+     * Constructor
+     * @param Object $controller Reference to the controller
+     */
+    public function __construct(&$controller)
+    {
+        $this->data = array();
+        $this->controller = &$controller;
     }
 
+    /**
+     * Destructor
+     */
     public function __destruct()
     {
+        unset($this->data);
+    }
 
+    /**
+     * Build the actual display and print it
+     */
+    abstract public function print_page();
+
+    /**
+     * Add data to the view, which is then accessible from within
+     * the print_page() function.
+     * @param Mixed $key Identifier for the data
+     * @param Mixed $value The data
+     */
+    public function add_data($key, &$value)
+    {
+        $this->data[$key] = &$value;
     }
 
 }
