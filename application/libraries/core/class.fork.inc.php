aba97f72ecc4d3e4d37ae1481d1684ce1222724f
Heinz Wiesinger <heinz@m2mobi.com>
2010-09-07 16:43:12 +0200

    application: Add process forking class, and avoid accidentally using it for the web (it's cli only functionality)

---
+++ application/libraries/core/class.fork.inc.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Process forking implementation
+ * @author M2Mobi, Heinz Wiesinger
+ */
+class Fork
+{
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Destructor
+     */
+    public function __destruct()
+    {
+
+    }
+
+    /**
+     * Start multiple parallel child processes
+     * @param Integer $number Amount of child processes to start
+     * @param Mixed $call The call that should be executed by the child processes
+     *      either "function" or "array('class','method')"
+     * @param Array $data Prepared array of data to be processed by the child processes
+     *      this array should be size() = $number
+     * @return Mixed Either false if run out of CLI context or an array of child process statuses
+     */
+    public static function fork($number, $call, $data)
+    {
+        global $cli;
+        if ($cli)
+        {
+            for($i = 0; $i < $number; ++$i)
+            {
+                $pids[$i] = pcntl_fork();
+
+                if(!$pids[$i])
+                {
+                    // child process
+                    call_user_func_array($call,$data[$i]);
+                    exit();
+                }
+            }
+
+            $result = SplFixedArray($number);
+
+            for($i = 0; $i < $number; ++$i)
+            {
+                pcntl_waitpid($pids[$i], $status, WUNTRACED);
+                $result[$i] = $status;
+            }
+
+            return $result;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+}
+
+?>
