6e76df1bf625bf5950b6b5059eb92efa916665d2
Heinz Wiesinger <heinz@m2mobi.com>
2010-09-23 21:52:22 +0200

    application: simpler include/require syntax

--- application/libraries/core/class.model.inc.php
+++ application/libraries/core/class.model.inc.php
@@ -12,8 +12,8 @@ abstract class Model
      */
     public function __construct()
     {
-        require_once(dirname(__FILE__) . "/../db/class.dbcon.inc.php");
-        require_once(dirname(__FILE__) . "/../db/class.query.inc.php");
+        require_once("class.dbcon.inc.php");
+        require_once("class.query.inc.php");
     }
 
     /**
