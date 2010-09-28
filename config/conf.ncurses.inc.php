72dd370213174c3d46d1386752e821b348f7765d
Heinz Wiesinger <heinz@m2mobi.com>
2010-09-28 09:50:08 +0200

    system: define common ncurses constants in conf.ncurses.inc.php

---
+++ config/conf.ncurses.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+define("NCURSES_KEY_ESCAPE", 27);
+
+define("NCURSES_KEY_Q", 113);
+
+define("NCURSES_KEY_CR", 13);
+define("NCURSES_KEY_LF", 10);
+
+define("NCURSES_KEY_TAB", 9);
+
+define("NCURSES_KEY_BS", 127);
+
+?>
