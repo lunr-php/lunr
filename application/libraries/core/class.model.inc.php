9aa4b646e5f480a8d21b3c016bd90dbbf500ed32
Heinz Wiesinger <heinz@m2mobi.com>
2010-11-19 18:21:53 +0100

    application: move dbcon instantiation from flights model   to core model.
      All other models need to be adjusted for that change and are
      currently broken

--- application/libraries/core/class.model.inc.php
+++ application/libraries/core/class.model.inc.php
@@ -8,12 +8,19 @@ abstract class Model
 {
 
     /**
+     * Reference to the database connection
+     * @var DBCon
+     */
+    protected $db;
+
+    /**
      * Constructor
      */
-    public function __construct()
+    public function __construct($db)
     {
         require_once("class.dbcon.inc.php");
         require_once("class.query.inc.php");
+        $this->db = DBCon::get_db_connection($db);
     }
 
     /**
@@ -21,7 +28,7 @@ abstract class Model
      */
     public function __destruct()
     {
-
+        unset($this->db);
     }
 
 }
