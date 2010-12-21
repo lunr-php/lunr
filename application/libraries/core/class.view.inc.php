88fec9cdfc3c3680279b89f6ca0f4ec4883dc856
Heinz Wiesinger <heinz@m2mobi.com>
2010-12-21 18:25:52 +0100

    system: Add view helper methods in core view

--- application/libraries/core/class.view.inc.php
+++ application/libraries/core/class.view.inc.php
@@ -53,6 +53,53 @@ abstract class View
         $this->data[$key] = &$value;
     }
 
+    /**
+     * Return base_url or attach given path to base_url
+     * @param String $path Path that should be attached to base_url (optional)
+     * @return String $return base_url (+ the given path, if given)
+     */
+    protected function base_url($path = "")
+    {
+        return $this->config['base_url'] . $path;
+    }
+
+    /**
+     * Return path to statics or attach given path to it
+     * @param String $path Path that should be attached to the statics path (optional)
+     * @return String $return path to statics (+ the given path, if given)
+     */
+    protected function statics($path = "")
+    {
+        return $this->base_url("statics/" . $path);
+    }
+
+    /**
+     * Return an alternating (eg. odd/even) CSS class name
+     * @param String $basename CSS base class name (without ending underscore or suffix)
+     * @param Integer $alternation_hint Integer counter indicating the alternation state
+     * @param String $suffix An alternative suffix if you don't want odd/even
+     * @return String $return The constructed CSS class name
+     */
+    protected function css_alternate($basename, $alternation_hint, $suffix = "")
+    {
+        if ($suffix == "")
+        {
+            if ( $alternation_hint % 2 == 0 )
+            {
+                $basename . '_even';
+            }
+            else
+            {
+               $basename . '_odd';
+            }
+        }
+        else
+        {
+            $basename . '_' . $suffix;
+        }
+        return $basename;
+    }
+
 }
 
 ?>
