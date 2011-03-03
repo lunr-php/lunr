35097b0414f5a8ab0d0263d598d25fbd76ba3759
Heinz Wiesinger <heinz@m2mobi.com>
2011-03-03 23:35:28 +0100

    system: code style fixes for M2DateTime

--- system/libraries/core/class.m2datetime.inc.php
+++ system/libraries/core/class.m2datetime.inc.php
@@ -1,12 +1,25 @@
 <?php
 
 /**
- * Date/Time related functions
+ * This file contains the class M2DateTime, which is a collection
+ * of commonly used Date and Time methods.
+ *
+ * PHP Version 5
  *
  * @category   Libraries
  * @package    Core
  * @subpackage Libraries
  * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ *
+ */
+
+/**
+ * Date/Time related functions
+ *
+ * @category   Libraries
+ * @package    Core
+ * @subpackage Libraries
  * @author     Heinz Wiesinger <heinz@m2mobi.com>
  *
  */
@@ -31,6 +44,7 @@ class M2DateTime
 
     /**
      * Return today's date (YYYY-MM-DD)
+     *
      * @return String Today's date
      */
     public static function today()
@@ -40,6 +54,7 @@ class M2DateTime
 
     /**
      * Return yesterday's date (YYYY-MM-DD)
+     *
      * @return String Tomorrow's date
      */
     public static function yesterday()
@@ -49,6 +64,7 @@ class M2DateTime
 
     /**
      * Return tomorrow's date (YYYY-MM-DD)
+     *
      * @return String Tomorrow's date
      */
     public static function tomorrow()
@@ -58,8 +74,11 @@ class M2DateTime
 
     /**
      * Return a date of a certain timeframe in the past/future
-     * @param String $delay Definition for a timeframe ("+1 day", "-10 minutes")
+     *
+     * @param String  $delay     Definition for a timeframe
+     *                           ("+1 day", "-10 minutes")
      * @param Integer $timestamp Base timestamp, now by default (optional)
+     *
      * @return String Delayed date
      */
     public static function delayed_date($delay, $timestamp = 0)
@@ -73,8 +92,11 @@ class M2DateTime
 
     /**
      * Return a timestamp of a certain timeframe in the past/future
-     * @param String $delay Definition for a timeframe ("+1 day", "-10 minutes")
+     *
+     * @param String  $delay     Definition for a timeframe
+     *                           ("+1 day", "-10 minutes")
      * @param Integer $timestamp Base timestamp, now by default (optional)
+     *
      * @return Integer Delayed timestamp
      */
     public static function delayed_timestamp($delay, $timestamp = 0)
@@ -88,8 +110,11 @@ class M2DateTime
 
     /**
      * Return a delayed datetime string
-     * @param String $delay Definition for a timeframe ("+1 day", "-10 minutes")
+     *
+     * @param String  $delay     Definition for a timeframe
+     *                           ("+1 day", "-10 minutes")
      * @param Integer $timestamp Base timestamp, now by default (optional)
+     *
      * @return String $return DateTime definition, format YYYY-MM-DD HH:MM:SS
      */
     public static function delayed_datetime($delay, $timestamp = 0)
@@ -103,6 +128,7 @@ class M2DateTime
 
     /**
      * Return the current time (HH:MM:SS)
+     *
      * @return String current time
      */
     public static function now()
@@ -112,7 +138,9 @@ class M2DateTime
 
     /**
      * Returns a MySQL compatible date definition
+     *
      * @param Integer $timestamp PHP-like Unix Timestamp
+     *
      * @return String $date Date as a string
      */
     public static function get_date($timestamp)
@@ -122,7 +150,9 @@ class M2DateTime
 
     /**
      * Returns a MySQL compatible time definition
+     *
      * @param Integer $timestamp PHP-like Unix Timestamp
+     *
      * @return String $time Time as a string
      */
     public static function get_time($timestamp)
@@ -132,7 +162,9 @@ class M2DateTime
 
     /**
      * Returns a MySQL compatible Date & Time definition
+     *
      * @param Integer $timestamp PHP-like Unix Timestamp (optional)
+     *
      * @return String $datetime Date & Time as a string
      */
     public static function get_datetime($timestamp = FALSE)
@@ -149,8 +181,11 @@ class M2DateTime
 
     /**
      * Return a date formatted as "DD MMM" (eg 05 Dec)
+     *
      * @param Integer $timestamp PHP-like Unix Timestamp (optional)
-     * @param String $locale The locale that should be used for the month names (optional, en_US by default)
+     * @param String  $locale    The locale that should be used for the month
+     *                           names (optional, en_US by default)
+     *
      * @return String $date Date as a string
      */
     public static function get_human_readable_date_short($timestamp = FALSE, $locale = "en_US")
@@ -168,7 +203,9 @@ class M2DateTime
 
     /**
      * Checks whether a given input string is a valid time definition
+     *
      * @param String $string Input String
+     *
      * @return Boolean $return True if it is valid, False otherwise
      */
     public static function is_time($string)
@@ -186,12 +223,18 @@ class M2DateTime
 
     /**
      * Checks whether a given input string is a valid date definition
+     *
      * @param String $string Input String
-     * @return Boolean $return True if it is a proper date String, False otherwise
+     *
+     * @return Boolean $return True if it is a proper date String,
+     *                         False otherwise
      */
     public static function is_date($string)
     {
-        if (preg_match("/^(\d{1,4})[\- \/ \.]((01|0[3-9]|1[012])[\- \/ \.](0[1-9]|[12][0-9]|3[01])|02[\- \/ \.](0[1-9]|1[0-9]|2[0-9]))$/", $string))
+        $feb    = "02[\- \/ \.](0[1-9]|1[0-9]|2[0-9])";
+        $others = "(01|0[3-9]|1[012])[\- \/ \.](0[1-9]|[12][0-9]|3[01])";
+
+        if (preg_match("/^(\d{1,4})[\- \/ \.]($others|$feb)$/", $string))
         {
             return TRUE;
         }
@@ -204,8 +247,10 @@ class M2DateTime
     /**
      * Compares two datetime strings and returns smaller or bigger
      * This function can be used for PHP's sorting functions
+     *
      * @param String $a DateTime String 1
      * @param String $b DateTime String 2
+     *
      * @return Integer -1 if $a < $b, 1 otherwise
      */
     public static function sort_compare_datetime($a, $b)
