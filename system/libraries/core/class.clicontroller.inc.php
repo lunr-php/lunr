668913a07e76b8c31af7634685078c2f397ced9d
Heinz Wiesinger <heinz@m2mobi.com>
2011-03-07 17:16:37 +0100

    system: Documentation and code style fixes for the CLIController

--- system/libraries/core/class.clicontroller.inc.php
+++ system/libraries/core/class.clicontroller.inc.php
@@ -1,8 +1,27 @@
 <?php
 
 /**
+ * This file contains an extension for the base controller
+ * class, which implements additional features specifically
+ * required by the CLI.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    Core
+ * @subpackage Libraries
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+
+/**
  * CLI Controller class
- * @author M2Mobi, Heinz Wiesinger
+ *
+ * @category   Libraries
+ * @package    Core
+ * @subpackage Libraries
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
  */
 abstract class CliController extends Controller
 {
@@ -12,7 +31,7 @@ abstract class CliController extends Controller
      */
     public function __construct()
     {
-
+        parent::__construct();
     }
 
     /**
@@ -20,13 +39,15 @@ abstract class CliController extends Controller
      */
     public function __destruct()
     {
-
+        parent::__destruct();
     }
 
     /**
      * Handle unimplemented calls
+     *
      * @param String $name Method name
      * @param array $arguments Arguments passed to the method
+     *
      * @return String JSON encoded error String
      */
     public function __call($name, $arguments)
@@ -36,6 +57,7 @@ abstract class CliController extends Controller
 
     /**
      * Default method as defined in conf.application.inc.php
+     * @return void
      */
     abstract public function index();
 
