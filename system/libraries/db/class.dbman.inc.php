22fd166eef119785f60ab7cdb6453f967a3fe685
Heinz Wiesinger <heinz@m2mobi.com>
2011-03-07 15:42:10 +0100

    system: Documentation and code style fixes for the dbman class

--- system/libraries/db/class.dbman.inc.php
+++ system/libraries/db/class.dbman.inc.php
@@ -1,8 +1,26 @@
 <?php
 
 /**
+ * This file contains a Database Connection Manager
+ * class, managing database read and write connections
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    Database
+ * @subpackage Libraries
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+
+/**
  * Database connection manager class
- * @author M2Mobi, Heinz Wiesinger
+ *
+ * @category   Libraries
+ * @package    Database
+ * @subpackage Libraries
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
  */
 class DBMan
 {
@@ -20,25 +38,11 @@ class DBMan
     private static $rw_instance;
 
     /**
-     * Constructor
-     */
-    private function __construct()
-    {
-
-    }
-
-    /**
-     * Destructor
-     */
-    public function __destruct()
-    {
-
-    }
-
-    /**
      * Return an instance of dbcon
-     * @param array $db Database configuration values
+     *
+     * @param array   $db       Database configuration values
      * @param Boolean $readonly Whether to return a readonly connection or not
+     *
      * @return DBCon Reference to the DBCon Singleton
      */
     public static function get_db_connection($db, $readonly = TRUE)
