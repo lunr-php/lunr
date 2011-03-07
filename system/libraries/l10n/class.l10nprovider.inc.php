71e6dfd0d395c2bf47218e1a1cc2ac8726403642
Heinz Wiesinger <heinz@m2mobi.com>
2011-03-07 12:53:19 +0100

    system: Documentation and style fixes for the L10nProvider class

--- system/libraries/l10n/class.l10nprovider.inc.php
+++ system/libraries/l10n/class.l10nprovider.inc.php
@@ -1,8 +1,28 @@
 <?php
 
 /**
+ * This file contains the abstract definition for a
+ * Localization Provider. A Localization Provider
+ * is a class accessing a low-level localization
+ * infrastructure.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    L10n
+ * @subpackage Libraries
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+
+/**
  * Abstract Localization Provider class
- * @author M2Mobi, Heinz Wiesinger
+ *
+ * @category   Libraries
+ * @package    L10n
+ * @subpackage Libraries
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
  */
 abstract class L10nProvider
 {
@@ -15,6 +35,7 @@ abstract class L10nProvider
 
     /**
      * Constructor
+     *
      * @param String $language POSIX locale definition
      */
     abstract public function __construct($language);
@@ -26,26 +47,32 @@ abstract class L10nProvider
 
     /**
      * Initialization method for setting up the provider
+     *
      * @param String $language POSIX locale definition
+     *
      * @return void
      */
     abstract protected function init($language);
 
     /**
      * Return a translated string
+     *
      * @param String $identifier Identifier for the requested string
-     * @param String $context Context information fot the requested string
+     * @param String $context    Context information fot the requested string
+     *
      * @return String $string Translated string, identifier by default
      */
     abstract public function lang($identifier, $context = "");
 
     /**
-     * Return a translated string, with proper singular/plural
-     * form
-     * @param String $singular Identifier for the singular version of the string
-     * @param String $plural Identifier for the plural version of the string
-     * @param Integer $amount The amount the translation should be based on
-     * @param String $context Context information fot the requested string
+     * Return a translated string, with proper singular/plural form
+     *
+     * @param String  $singular Identifier for the singular version of the
+     *                          string
+     * @param String  $plural   Identifier for the plural version of the string
+     * @param Integer $amount   The amount the translation should be based on
+     * @param String  $context  Context information fot the requested string
+     *
      * @return String $string Translated string, identifier by default
      */
     abstract public function nlang($singular, $plural, $amount, $context = "");
