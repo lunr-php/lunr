d639e0ad71459d04ece664a2562605a25b9d5c08
Heinz Wiesinger <heinz@m2mobi.com>
2011-03-08 09:32:49 +0100

    system: Documentation and style fixes for the Process class

--- system/libraries/core/class.process.inc.php
+++ system/libraries/core/class.process.inc.php
@@ -1,34 +1,41 @@
 <?php
 
 /**
+ * This file contains the Process class, which contains helper
+ * methods to handle external processes or ease script-internal
+ * process handling.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    Core
+ * @subpackage Libraries
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+
+/**
  * Process control class
- * @author M2Mobi, Heinz Wiesinger
+ *
+ * @category   Libraries
+ * @package    Core
+ * @subpackage Libraries
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
  */
 class Process
 {
 
     /**
-     * Constructor
-     */
-    public function __construct()
-    {
-
-    }
-
-    /**
-     * Destructor
-     */
-    public function __destruct()
-    {
-
-    }
-
-    /**
      * Run a system command from inside PHP
-     * @param String $cmd The command to run
-     * @param String $out The location where output should be redirected too
-     * @param Boolean $append Whether to append to the output location, or to replace it
+     *
+     * @param String  $cmd            The command to run
+     * @param String  $out            The location where output should
+     *                                be redirected too
+     * @param Boolean $append         Whether to append to the output
+     *                                location, or to replace it
      * @param Boolean $error_redirect Whether to redirect STDERR to STDOUT
+     *
      * @return String $return Last line of the STDOUT output of the command
      */
     public static function run($cmd, $out = "", $append = FALSE, $error_redirect = TRUE)
@@ -54,10 +61,14 @@ class Process
 
     /**
      * Run a system command from inside PHP in background
-     * @param String $cmd The command to run
-     * @param String $out The location where output should be redirected too
-     * @param Boolean $append Whether to append to the output location, or to replace it
+     *
+     * @param String  $cmd            The command to run
+     * @param String  $out            The location where output should
+     *                                be redirected too
+     * @param Boolean $append         Whether to append to the output
+     *                                location, or to replace it
      * @param Boolean $error_redirect Whether to redirect STDERR to STDOUT
+     *
      * @return void
      */
     public static function run_bg($cmd, $out = "", $append = FALSE, $error_redirect = TRUE)
@@ -89,15 +100,21 @@ class Process
 
     /**
      * Start multiple parallel child processes
-     * WARNING: make sure to not reuse an already existing DB-Connection established by the parent
-     * in the children. Best would be to not establish a DB-Connection in the parent at all.
+     * WARNING: make sure to not reuse an already existing DB-Connection
+     * established by the parent in the children. Best would be to not
+     * establish a DB-Connection in the parent at all.
      * WARNING: Do NOT use this for a web process!!!!
+     *
      * @param Integer $number Amount of child processes to start
-     * @param Mixed $call The call that should be executed by the child processes
-     *      either "function" or "array('class','method')"
-     * @param Array $data Prepared array of data to be processed by the child processes
-     *      this array should be size() = $number
-     * @return Mixed Either false if run out of CLI context or an array of child process statuses
+     * @param Mixed   $call   The call that should be executed by the child
+     *                        processes either "function" or
+     *                        "array('class','method')"
+     * @param Array   &$data  Prepared array of data to be processed by the
+     *                        child processes this array should be size() =
+     *                        $number
+     *
+     * @return Mixed $result Either false if run out of CLI context or an
+     *                       array of child process statuses
      */
     public static function fork($number, $call, &$data)
     {
@@ -113,15 +130,16 @@ class Process
                     // child process
                     if (is_array($data[$i]))
                     {
-                        call_user_func_array($call,$data[$i]);
+                        call_user_func_array($call, $data[$i]);
                     }
-                    elseif (is_object($data[$i]) && ($data[$i] instanceof SplFixedArray))
+                    elseif (is_object($data[$i])
+                        && ($data[$i] instanceof SplFixedArray))
                     {
-                        call_user_func_array($call,$data[$i]->toArray());
+                        call_user_func_array($call, $data[$i]->toArray());
                     }
                     else
                     {
-                        call_user_func($call,$data[$i]);
+                        call_user_func($call, $data[$i]);
                     }
                     exit();
                 }
