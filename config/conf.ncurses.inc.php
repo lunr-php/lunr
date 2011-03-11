c76fd3f655116724b1e0058dfcfb8b2fccd4835e
Heinz Wiesinger <heinz@m2mobi.com>
2011-03-11 11:13:13 +0100

    config: Documentation and style fixes for the ncurses config

--- config/conf.ncurses.inc.php
+++ config/conf.ncurses.inc.php
@@ -1,14 +1,45 @@
 <?php
 
+/**
+ * ncurses Constant Definitions
+ *
+ * PHP Version 5.3
+ *
+ * @category   Config
+ * @package    CLI
+ * @subpackage Config
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+
+/**
+ * key 'Esc'
+ */
 define("NCURSES_KEY_ESCAPE", 27);
 
+/**
+ * key 'Q'
+ */
 define("NCURSES_KEY_Q", 113);
 
+/**
+ * Carriage return character
+ */
 define("NCURSES_KEY_CR", 13);
+
+/**
+ * Line feed character
+ */
 define("NCURSES_KEY_LF", 10);
 
+/**
+ * key 'tab'
+ */
 define("NCURSES_KEY_TAB", 9);
 
+/**
+ * key 'backspace'
+ */
 define("NCURSES_KEY_BS", 127);
 
 ?>
