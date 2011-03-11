d94cf7bfe8abc53f3cd18c01244561e290b4196a
Heinz Wiesinger <heinz@m2mobi.com>
2011-03-11 11:07:13 +0100

    config: Documentation and style fixes for the MySQL config

--- config/conf.mysql.inc.php
+++ config/conf.mysql.inc.php
@@ -1,6 +1,25 @@
 <?php
 
+/**
+ * MySQL/MariaDB Constant Definitions
+ *
+ * PHP Version 5.3
+ *
+ * @category   Config
+ * @package    Database
+ * @subpackage Config
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+
+/**
+ * Error: Duplicate Primary Key
+ */
 define("DUPLICATE_PRIMARY_KEY", 1062);
+
+/**
+ * Error: Foreign key constraint failed
+ */
 define("FOREING_KEY_CONTRAINT_FAILED", 1216);
 
 ?>
