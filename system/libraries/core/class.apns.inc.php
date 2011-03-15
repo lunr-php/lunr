8ddeeaeca7ba4b1df0a8635c72a4297d03006da8
Julio Foulquie <julio@m2mobi.com>
2011-03-15 17:41:22 +0100

    system: make the APNS library error free against phpcs.

--- system/libraries/core/class.apns.inc.php
+++ system/libraries/core/class.apns.inc.php
@@ -1,8 +1,30 @@
 <?php
 
 /**
+* This file contains the class APNS which stands for Apple Push Notifications
+* System. This class allows to send push notifications to Apple devices and
+* manage the list of devices which are recorded.
+*
+* PHP Version 5.3
+*
+* @category   Libraries
+* @package    Core
+* @subpackage Libraries
+* @author     M2Mobi <info@m2mobi.com>
+* @author     Julio Foulquié <julio@m2mobi.com>
+* @author     Heinz Wiesinger <heinz@m2mobi.com>
+*
+*/
+
+/**
  * Apple Push Notifications System Library
- * @author M2Mobi, Julio Foulquié, Heinz Wiesinger
+ *
+ * @category   Libraries
+ * @package    Core
+ * @subpackage Libraries
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Julio Foulquié <julio@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
  */
 class APNS
 {
@@ -41,80 +63,108 @@ class APNS
     }
 
     /**
-    * Send a push notification to device based on the given device token
-    * @param String $device_token The device token which identifies the device
-    * @param Array $payload The payload that'll be sent already formatted
-    * @return Boolean, TRUE on success, FALSE otherwise
-    */
+     * Send a push notification to device based on the given device token
+     *
+     * @param String $device_token The device token which identifies the device
+     * @param Array  $payload      The payload that'll be sent already
+     *                             formatted
+     *
+     * @return Boolean, TRUE on success, FALSE otherwise
+     */
     public function send_apple_push($device_token, $payload)
     {
         global $config, $JSON;
 
         $ctx = stream_context_create();
-        stream_context_set_option($ctx, 'ssl', 'local_cert', $config['apns']['cert']);
-        stream_context_set_option($ctx, 'ssl', 'passphrase', $config['apns']['cert']['pass']);
-        $fp = stream_socket_client($config['apns']['push'], $err, $errstr, 60, STREAM_CLIENT_CONNECT, $ctx);
+        stream_context_set_option(
+            $ctx, 'ssl', 'local_cert', $config['apns']['cert']
+        );
+        stream_context_set_option(
+            $ctx, 'ssl', 'passphrase', $config['apns']['cert']['pass']
+        );
+        $fp = stream_socket_client(
+            $config['apns']['push'], $err, $errstr, 60, STREAM_CLIENT_CONNECT, $ctx
+        );
         if(!$fp)
         {
-            Output::error("Error while opening socket: $err\n\n", $config['apns']['log']);
+            Output::error(
+                "Error while opening socket: $err\n\n", $config['apns']['log']
+            );
             return FALSE;
         }
         $json_payload = json_encode($payload);
-//         $identifier = '0011';
-//         $expiry_date = '';
+        //$identifier = '0011';
+        //$expiry_date = '';
         //Simple notification format
-        $msg = chr(0) . pack("n", 32) . pack('H*', str_replace(' ', '', $device_token)) . pack("n", strlen($json_payload)) . $json_payload;
+        $msg = chr(0)
+            . pack("n", 32)
+            . pack('H*', str_replace(' ', '', $device_token))
+            . pack("n", strlen($json_payload)) . $json_payload;
 
         //Enhanced notification format
-//         $msg = chr(0) . pack("n", $identifier) . pack("n", $expiry_date) . pack("n", 32) . pack('H*', str_replace(' ', '', $device_token)) . pack("n", strlen($json_payload)) . $json_payload;
+        //$msg = chr(0)
+            //. pack("n", $identifier)
+            //. pack("n", $expiry_date)
+            //. pack("n", 32)
+            //. pack('H*', str_replace(' ', '', $device_token))
+            //. pack("n", strlen($json_payload)) . $json_payload;
 
         if(!fwrite($fp, $msg))
         {
-            Output::error("Error while unserializing\n\n", $config['apns']['log']);
+            Output::error(
+                "Error while unserializing\n\n", $config['apns']['log']
+            );
             return FALSE;
         }
-//         $response = "";
-//         while (!feof($fp))
-//         {
-//             $response .= fgets($fp, 6);
-//         }
+        //$response = "";
+        //while (!feof($fp))
+        //{
+            //$response .= fgets($fp, 6);
+        //}
         if(!fclose($fp))
         {
-            Output::error("Error while closing socket\n\n", $config['apns']['log']);
-//             return FALSE;
+            Output::error(
+                "Error while closing socket\n\n", $config['apns']['log']
+            );
+            //return FALSE;
         }
-//         if(!strlen($res;ponse))
-//         {
-//             //TODO: Check this unpack
-//             echo "Response from Apple: ";
-//             var_dump($response);
-//             $result[] = unpack("C1/n1status/N4identifier", $response);
-// //             var_dump($result);
-//         }
-//         else
-//         {
-//             return TRUE;
-//         }
+        //if(!strlen($res;ponse))
+        //{
+        ////TODO: Check this unpack
+            //echo "Response from Apple: ";
+            //var_dump($response);
+            //$result[] = unpack("C1/n1status/N4identifier", $response);
+            ////var_dump($result);
+        //}
+        //else
+        //{
+            //return TRUE;
+        //}
 
-//         if($result['status'] == 0)
-//         {
-//             return TRUE;
-//         }
-//         else
-//         {
-// //             $this->process_apple_error($response);
-//             return FALSE;
-//         }
-        Output::error("Notification sent: $json_payload \n\n", $config['apns']['log']);
+        //if($result['status'] == 0)
+        //{
+            //return TRUE;
+        //}
+        //else
+        //{
+            ////$this->process_apple_error($response);
+            //return FALSE;
+        //}
+        Output::error(
+            "Notification sent: $json_payload \n\n", $config['apns']['log']
+        );
         return TRUE;
     }
 
     /**
-    * Get a list with the devices that have no longer the application installed so
-    * we shouldn't keep sending them push notifications. Otherwise Steve Jobs
+    * Get a list with the devices that have no longer the application installed
+    * so we shouldn't keep sending them push notifications. Otherwise Steve Jobs
     * will become angry and will rape your family and kill your pets!!!
+    *
     * WARNING: NOT FINISHED (NEITHER USED) YET!
-    * TODO: Add a cli function for removing the device ID of the users who uninstall the app
+    * TODO: Add a cli function for removing the device ID of the users who
+    *       uninstall the app
+    *
     * @return Array, An array with all the device tokens that should be removed
     */
     public function get_apple_feedback()
@@ -122,11 +172,19 @@ class APNS
         //connect to the APNS feedback servers
         //make sure you're using the right dev/production server & cert combo!
         $ctx = stream_context_create();
-        stream_context_set_option($ctx, 'ssl', 'local_cert', $config['apns']['cert']);
-        stream_context_set_option($ctx, 'ssl', 'passphrase', $config['apns']['cert']['pass']);
+        stream_context_set_option(
+            $ctx, 'ssl', 'local_cert', $config['apns']['cert']
+        );
+        stream_context_set_option(
+            $ctx, 'ssl', 'passphrase', $config['apns']['cert']['pass']
+        );
 
         echo "CONNECTING TO FEEDBACK APNS\t\t";
-        $apns = stream_socket_client($config['apns']['feedback'], $errcode, $errstr, 60, STREAM_CLIENT_CONNECT, $ctx);
+        $apns = stream_socket_client(
+            $config['apns']['feedback'], $errcode,
+            $errstr, 60,
+            STREAM_CLIENT_CONNECT, $ctx
+        );
         if (!$apns)
         {
             echo "[FAIL]\nError $errcode: $errstr\n";
@@ -138,21 +196,24 @@ class APNS
 
         $feedback_tokens = array ();
         //and read the data on the connection:
-        while ( ! feof ( $apns ) ) {
-            $data = fread ( $apns, 38 );
-            if (strlen ( $data ))
+        while (!feof($apns))
+        {
+            $data = fread($apns, 38);
+            if (strlen($data))
             {
-                $feedback_tokens[] = unpack("N1timestamp/n1length/H*devtoken", $data);
+                $feedback_tokens[] =
+                    unpack("N1timestamp/n1length/H*devtoken", $data);
             }
         }
         fclose($apns);
-
-        var_dump($feedback_tokens);
+        //var_dump($feedback_tokens);
     }
 
     /**
     * Reads the response from APNS and logs the error accordingly
+    *
     * @param String $response The response from APNS
+    *
     * @return void
     */
     public function process_apple_error($response)
@@ -161,7 +222,10 @@ class APNS
         {
             $response[1] = 9;
         }
-        Output::error("Push notification '$response' rejected by APNS. Reason: " . $this->error[$response[1]], $config['apns']['log']);
+        Output::error(
+            "Push notification '$response' rejected by APNS. Reason: "
+            . $this->error[$response[1]], $config['apns']['log']
+        );
     }
 }
 
