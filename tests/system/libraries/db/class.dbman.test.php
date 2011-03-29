937a4b50e4ddbae2a650b21cdd9c67254f390c84
Heinz Wiesinger <heinz@m2mobi.com>
2011-03-29 17:15:25 +0200

    tests: Add unit test for DBMan class.

---
+++ tests/system/libraries/db/class.dbman.test.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once("class.dbman.inc.php");
+require_once("class.dbcon.inc.php");
+require_once("conf.database.inc.php");
+
+/**
+ * This tests Lunr's DBMan class
+ * @covers DBMan
+ */
+class DBManTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test the static function get_db_connection()
+     * @covers DBMan::get_db_connection
+     */
+    public function testGetDbConnection()
+    {
+        global $db;
+        $readonly = DBMan::get_db_connection($db);
+
+        $this->assertTrue($readonly->is_readonly());
+        unset($readonly);
+
+        $readonly = DBMan::get_db_connection($db, TRUE);
+
+        $this->assertTrue($readonly->is_readonly());
+        unset($readonly);
+
+        $readonly = DBMan::get_db_connection($db, FALSE);
+
+        $this->assertFalse($readonly->is_readonly());
+        unset($readonly);
+    }
+
+}
+
+?>
