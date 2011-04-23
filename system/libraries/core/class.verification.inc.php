89a4d2c31d21c70f0f38fab56f7846a3393bfd9f
Heinz Wiesinger <heinz@m2mobi.com>
2011-04-23 18:56:20 +0200

    system: Small extensions for the verification class.
    Implement a safe-guard for non-implemented checks and a first check
    for character length.

--- system/libraries/core/class.verification.inc.php
+++ system/libraries/core/class.verification.inc.php
@@ -26,6 +26,28 @@ class Verification
 {
 
     /**
+     * Catch unimplemented static functions.
+     *
+     * @param String $method    Method name
+     * @param array  $arguments Arguments to that static method
+     *
+     * @return Boolean $return Return value of the forwarded check,
+     *                         or FALSE if check not implemented.
+     */
+    public static function __callStatic($method, $arguments)
+    {
+        if (substr($method, 0, 12) == "check_length")
+        {
+            $length = substr($method, 12);
+            return self::check_length($length, $arguments[0]);
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+    /**
      * Verify an input array against a defined ruleset.
      *
      * @param String $identifier Identifier for the rule-checking
@@ -37,7 +59,7 @@ class Verification
      */
     public static function verify_array_ruleset($identifier, &$input, &$ruleset)
     {
-        if (trim($identifier) != "")
+        if (trim($identifier) == "")
         {
             Output::error("Can't verify input. Empty Identifier!'");
             return FALSE;
@@ -46,6 +68,20 @@ class Verification
         if (!is_array($input) || !is_array($ruleset))
         {
             Output::error("Can't verify input. Invalid input!'");
+            return FALSE;
+        }
+
+        // Check that input matches with the defined ruleset
+        $input_elements = array_keys($input);
+        $ruleset_elements = array_keys($ruleset);
+        $unhandled_elements = array_diff($ruleset_elements, $input_elements);
+
+        if ($unhandled_elements != array())
+        {
+            foreach ($unhandled_elements as $value)
+            {
+                Output::error("Input validation '$identifier': Ruleset for non-existing key '$value'!");
+            }
             return FALSE;
         }
 
@@ -61,9 +97,9 @@ class Verification
                 {
                     foreach ($ruleset[$key] as &$rule)
                     {
-                        if (call_user_func("self::check_" . $rule) === FALSE)
+                        if (call_user_func("self::check_" . $rule, $value) === FALSE)
                         {
-                            Output::error("Input validation '$identifier': Rule '$rule' failed!");
+                            Output::error("Input validation '$identifier': Rule '$rule' failed for '$key'!");
                             return FALSE;
                         }
                     }
@@ -71,22 +107,42 @@ class Verification
                 }
                 else
                 {
-                    if (call_user_func("self::check_" . $ruleset[$key]) === FALSE)
+                    if (call_user_func("self::check_" . $ruleset[$key], $value) === FALSE)
                     {
-                        Output::error("Input validation '$identifier': Rule '" . $ruleset[$key] . "' failed!");
+                        Output::error("Input validation '$identifier': Rule '" . $ruleset[$key] . "' failed for '$key'!");
                         return FALSE;
                     }
                 }
             }
             else
             {
-                Output::error("Input validation '$identifier': Unhandled Array Element!");
+                Output::error("Input validation '$identifier': Unhandled Array Element '$key'!");
                 return FALSE;
             }
         }
 
         //Everything looks good
         return TRUE;
+    }
+
+    /**
+     * Check character length of an input.
+     *
+     * @param Integer $length The length to check for
+     * @param mixed   &$value The input to check
+     *
+     * @return Boolean $return TRUE if size matches, FALSE otherwise.
+     */
+    public static function check_length($length, &$value)
+    {
+        if (strlen($value) == $length)
+        {
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
     }
 
 }
