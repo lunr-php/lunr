f38b6841661aedfa5bc1d17a358bf1a257bb0bee
Heinz Wiesinger <heinz@m2mobi.com>
2011-05-23 18:12:30 +0200

    system: Don't reference a parent for {Cli,Web}Controller

--- system/libraries/core/class.webcontroller.inc.php
+++ system/libraries/core/class.webcontroller.inc.php
@@ -31,7 +31,7 @@ abstract class WebController implements ControllerInterface
      */
     public function __construct()
     {
-        parent::__construct();
+
     }
 
     /**
@@ -39,7 +39,7 @@ abstract class WebController implements ControllerInterface
      */
     public function __destruct()
     {
-        parent::__destruct();
+
     }
 
     /**
