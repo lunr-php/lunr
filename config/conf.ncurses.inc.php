57621239f772445a248aee95d2f3b41a4dcc1ca9
Heinz Wiesinger <heinz@m2mobi.com>
2011-05-23 17:28:49 +0200

    application: Fix phpcs issues in config files

--- config/conf.ncurses.inc.php
+++ config/conf.ncurses.inc.php
@@ -15,31 +15,31 @@
 /**
  * key 'Esc'
  */
-define("NCURSES_KEY_ESCAPE", 27);
+define('NCURSES_KEY_ESCAPE', 27);
 
 /**
  * key 'Q'
  */
-define("NCURSES_KEY_Q", 113);
+define('NCURSES_KEY_Q', 113);
 
 /**
  * Carriage return character
  */
-define("NCURSES_KEY_CR", 13);
+define('NCURSES_KEY_CR', 13);
 
 /**
  * Line feed character
  */
-define("NCURSES_KEY_LF", 10);
+define('NCURSES_KEY_LF', 10);
 
 /**
  * key 'tab'
  */
-define("NCURSES_KEY_TAB", 9);
+define('NCURSES_KEY_TAB', 9);
 
 /**
  * key 'backspace'
  */
-define("NCURSES_KEY_BS", 127);
+define('NCURSES_KEY_BS', 127);
 
 ?>
