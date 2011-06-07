1e0f62ea353ebf2b6055479a7e16e5a7fca10614
Heinz Wiesinger <heinz@m2mobi.com>
2011-06-07 16:09:45 +0200

    tests: Add first unittests for verification class

---
+++ tests/system/libraries/core/class.verification.test.php
@@ -0,0 +1,76 @@
+<?php
+
+use Lunr\Libraries\Core\Verification;
+
+/**
+ * This tests Lunr's Verification class
+ * @covers Lunr\Libraries\Core\Verification
+ */
+class VerificationTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test the is_ignore part of __callStatic
+     * @covers Lunr\Libraries\Core\Verification::__callStatic
+     */
+    public function testIsIgnore()
+    {
+        $this->assertTrue(Verification::is_ignore());
+    }
+
+    /**
+     * Test the not implemented part of __callStatic
+     * @covers Lunr\Libraries\Core\Verification::__callStatic
+     */
+    public function testIsNotImplemented()
+    {
+        $this->assertFalse(Verification::graftznwiq3rz89());
+    }
+
+    /**
+     * Test the static function is_length()
+     * @dataProvider validLengthProvider
+     * @covers Lunr\Libraries\Core\Verification::is_length
+     */
+    public function testValidIsLength($length, $value)
+    {
+        $result = Verification::is_length($length, $value);
+
+        $this->assertTrue($result);
+
+        $method = "is_length" . $length;
+        $result = Verification::$method($value);
+
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Test the static function is_length()
+     * @dataProvider invalidLengthProvider
+     * @covers Lunr\Libraries\Core\Verification::is_length
+     */
+    public function testInvalidIsLength($length, $value)
+    {
+        $result = Verification::is_length($length, $value);
+
+        $this->assertFalse($result);
+
+        $method = "is_length" . $length;
+        $result = Verification::$method($value);
+
+        $this->assertFalse($result);
+    }
+
+    public function validLengthProvider()
+    {
+        return array(array(1,"a"), array(5,"heinz"), array(10,"transcript"));
+    }
+
+    public function invalidLengthProvider()
+    {
+        return array(array(2,"a"), array(3,"heinz"), array(76,"transcript"));
+    }
+
+}
+
+?>
