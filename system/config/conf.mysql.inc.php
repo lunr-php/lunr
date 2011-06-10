bc771929b9de0bb46500fbc9a296eb13acd22a5a
Heinz Wiesinger <heinz@m2mobi.com>
2011-06-10 17:26:13 +0200

    system: Move mysql/ncurses constant configs to system

--- system/config/conf.mysql.inc.php
+++ system/config/conf.mysql.inc.php
@@ -15,11 +15,11 @@
 /**
  * Error: Duplicate Primary Key
  */
-define('DUPLICATE_PRIMARY_KEY', 1062);
+define('MYSQL_DUPLICATE_PRIMARY_KEY', 1062);
 
 /**
  * Error: Foreign key constraint failed
  */
-define('FOREING_KEY_CONTRAINT_FAILED', 1216);
+define('MYSQL_FOREIGN_KEY_CONTRAINT_FAILED', 1216);
 
 ?>
