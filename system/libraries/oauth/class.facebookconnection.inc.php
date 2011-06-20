74a02e16fcdd576857385b8e0d74785893e86f3e
Javier Negre <javi@nulaz.com>
2011-06-20 12:16:29 +0200

    system: draft for Facebook Connection
    First implementation for get_request_token and get_access_token

--- system/libraries/oauth/class.facebookconnection.inc.php
+++ system/libraries/oauth/class.facebookconnection.inc.php
@@ -22,7 +22,7 @@
  * @author     M2Mobi <info@m2mobi.com>
  * @author     Heinz Wiesinger <heinz@m2mobi.com>
  */
-class FacebookConnection implements OAuthConnection
+class FacebookConnection implements OAuthConnectionInterface
 {
 
     private $token;
@@ -35,6 +35,41 @@ class FacebookConnection implements OAuthConnection
     public function __destruct()
     {
         unset($this->token);
+    }
+
+    public function get_request_token($callback)
+    {
+        global $config;
+
+        $state = md5(uniqid(rand(), TRUE));
+
+        $url = $config['social']['facebook']['request_token_url']
+            . '?client_id=' . $config['social']['facebook']['client_id']
+            . '&redirect_uri=' . urlencode($callback)
+            . '&state=' . $state
+            . '&display=wap';
+
+        return array(
+                'state' => $state,
+                'url' => $url
+            );
+    }
+
+    public function get_access_token ($oauth_token, $request_token_secret)
+    {
+        global $config;
+
+        $token_url = $config['social']['facebook']['access_token_url']
+            . "?client_id=" . $config['social']['facebook']['client_id']
+            . "&redirect_uri=" . urlencode($request_token_secret)
+            . "&client_secret=" . $config['social']['facebook']['app_secret']
+            . "&code=" . $oauth_token;
+
+        $response = file_get_contents($token_url);
+        $params = null;
+        parse_str($response, $params);
+
+        return $params;
     }
 
     public function login()
