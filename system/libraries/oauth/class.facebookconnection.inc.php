0197af6a7ad2d9bdeb9e428c0130d3c3582a5f5e
Julio Foulquie <julio@nulaz.com>
2011-07-04 17:50:28 +0200

    oauth: implement get_profile_info and post_message for Facebook.

--- system/libraries/oauth/class.facebookconnection.inc.php
+++ system/libraries/oauth/class.facebookconnection.inc.php
@@ -11,6 +11,7 @@
  * @subpackage Libraries
  * @author     M2Mobi <info@m2mobi.com>
  * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @author     Julio Foulquié <julio@m2mobi.com>
  */
 
 /**
@@ -21,29 +22,52 @@
  * @subpackage Libraries
  * @author     M2Mobi <info@m2mobi.com>
  * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @author     Julio Foulquié <julio@m2mobi.com>
  */
 class FacebookConnection implements OAuthConnectionInterface
 {
 
+    const NETWORK = 'facebook';
+
+    /**
+     * API access token
+     * @var String
+     */
     private $token;
 
+    /**
+     * Constructor.
+     *
+     * @param String $token User access token
+     */
     public function __construct($token)
     {
         $this->token = $token;
     }
 
+    /**
+     * Destructor.
+     */
     public function __destruct()
     {
         unset($this->token);
     }
 
+    /**
+     * Get the request token from the Facebook.
+     *
+     * @param String $callback URL to receive the callback from the OAuth provider
+     *
+     * @return Array Array containing the 'oauth_token_secret' and the
+     *              'request_token_secret' on success and FALSE otherwise
+     */
     public function get_request_token($callback)
     {
         global $config;
 
         $state = md5(uniqid(rand(), TRUE));
 
-        $url = $config['social']['facebook']['request_token_url']
+        $url = $config['social'][NETWORK]['request_token_url']
             . '?client_id=' . $config['social']['facebook']['client_id']
             . '&redirect_uri=' . urlencode($callback)
             . '&state=' . $state
@@ -55,14 +79,23 @@ class FacebookConnection implements OAuthConnectionInterface
             );
     }
 
-    public function get_access_token ($oauth_token, $request_token_secret)
+    /**
+     * Get access token from Facebook.
+     *
+     * @param String $oauth_token          Oauth token
+     * @param String $request_token_secret Request token secret
+     *
+     * @return Array Array containing the 'oauth token' and the 'oauth token secret',
+     *               FALSE on failure.
+     */
+    public function get_access_token($oauth_token, $url_back)
     {
         global $config;
 
-        $token_url = $config['social']['facebook']['access_token_url']
-            . "?client_id=" . $config['social']['facebook']['client_id']
-            . "&redirect_uri=" . urlencode($request_token_secret)
-            . "&client_secret=" . $config['social']['facebook']['app_secret']
+        $token_url = $config['social'][NETWORK]['access_token_url']
+            . "?client_id=" . $config['social'][NETWORK]['client_id']
+            . "&redirect_uri=" . urlencode($url_back)
+            . "&client_secret=" . $config['social'][NETWORK]['app_secret']
             . "&code=" . $oauth_token;
 
         $response = file_get_contents($token_url);
@@ -72,19 +105,40 @@ class FacebookConnection implements OAuthConnectionInterface
         return $params;
     }
 
-    public function login()
+    /**
+     * Get user profile info from Facebook.
+     *
+     * @param String $access_oauth_token  Oauth token
+     * @param String $access_token_secret Unused for Facebook
+     *
+     * @return Array Array containing the user profile information, FALSE otherwise
+     */
+    public function get_user_info($access_token, $access_token_secret = '')
     {
+        global $config;
 
+        $url = $config['social'][NETWORK]['own_profile_url'] . $access_token;
+        return file_get_contents($url);
     }
 
-    public function get_user_info()
+    /**
+     * Post a message to Facebook
+     *
+     * @param String $oauth_token         Oauth token
+     * @param String $msg                 SocialMessage object already filled
+     * @param String $access_token_secret Unused for Facebook
+     *
+     * @return Array Array containing the 'oauth token' and the 'oauth token secret',
+     *               FALSE otherwise.
+     */
+    public function post_message($access_token, $message, $access_token_secret = '')
     {
+        global $config;
 
-    }
+        $curl = new Curl();
+        $params = array('access_token' => $access_token, 'message' => $message->message);
 
-    public function post_message()
-    {
-
+        return $curl->simple_post($config['social'][NETWORK]['share_url'], $params);
     }
 }
 
