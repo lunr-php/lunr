2081bb10726ff99ec1a15da08151f0c6ef79f0cd
Jose Viso <jose@m2mobi.com>
2011-07-19 12:36:41 +0200

    system: C2DM - Add config file support and recover_auth_token function

--- system/libraries/core/class.c2dm.inc.php
+++ system/libraries/core/class.c2dm.inc.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
-* This file contains the class C2DM which stands for Android Push Notifications
-* System. This class allows to send push notifications to Android devices.
+* This file contains the class C2DM which stands for Android Cloud To Device Messaging
+* framework. This class allows to send push notifications to Android devices.
 *
 * PHP Version 5.3
 *
@@ -18,7 +18,7 @@ namespace Lunr\Libraries\Core;
 use Lunr\Libraries\Core\Curl;
 
 /**
- * Android Push Notifications System Library
+ * Android (C2DM) Push Notifications System Library
  *
  * @category   Libraries
  * @package    Core
@@ -34,7 +34,8 @@ class C2DM
      */
     public function __construct()
     {
-
+        global $config;
+        require_once('conf.c2dm.inc.php');
     }
 
     /**
@@ -55,10 +56,11 @@ class C2DM
      *
      * @return String authToken, FALSE otherwise
      */
-    public function get_auth_token($username, $password, $source='test', $service='ac2dm')
+    public function get_auth_token($username, $password, $source='', $service='ac2dm')
     {
-        $url = 'https://www.google.com/accounts/ClientLogin';
-        $post_fields = 'accountType=' . urlencode('HOSTED_OR_GOOGLE')
+        global $config;
+
+        $post_fields = 'accountType=' . urlencode($config['c2dm']['account_type'])
             . '&Email=' . urlencode($username)
             . '&Passwd=' . urlencode($password)
             . '&source=' . urlencode($source)
@@ -66,14 +68,14 @@ class C2DM
 
         $curl = new Curl();
         $curl->set_option('HEADER', TRUE);
-        $response = $curl->simple_post($url, $post_fields);
+        $response = $curl->simple_post($config['c2dm']['request_token_url'], $post_fields);
 
         if (strpos($response, '200 OK') === false)
         {
             return FALSE;
         }
 
-        // Look for the auth code
+        // Look for the authToken
         preg_match('/(Auth=)([\w|-]+)/', $response, $matches);
 
         if (!$matches[2])
@@ -82,26 +84,37 @@ class C2DM
             return FALSE;
         }
 
+        // Store in a file the authToken
+        $reffile = fopen(dirname(__FILE__) . "/../../../token_string.txt", "w");
+        fwrite ($reffile, $matches[2]);
+        fclose($reffile);
+
         unset($curl);
         return $matches[2];
-
     }
 
     /**
      * Send Android push notification based on registration ID and authToken.
      *
      * @param String $registrationID The registration ID retrieved from the app on the phone
-     * @param String $authToken The authorization token
      * @param String $message The message that will be sent.
      *
      * @return mixed The message ID on success, FALSE otherwise
      */
-    public function send_android_push($registrationID, $authToken, $message)
+    public function send_android_push($registrationID, $message)
     {
-        $url = 'https://android.apis.google.com/c2dm/send';
-        $msgType = "important";
+        global $config;
+
+        $authToken = $this->revocer_auth_token();
+
+        if(!$authToken)
+        {
+            $authToken = $this->get_auth_token($config['c2dm']['username'], $config['c2dm']['password']);
+        }
 
         $headers = array('Authorization: GoogleLogin auth=' . $authToken);
+        $msgType = $config['c2dm']['msg_type'];
+
         $data = array(
                     'registration_id' => $registrationID,
                     'collapse_key' => $msgType,
@@ -112,28 +125,25 @@ class C2DM
         $curl->set_option('HEADER', TRUE);
         $curl->set_option('HTTPHEADER', $headers);
 
-        $returned_data = $curl->simple_post($url, $data);
+        $returned_data = $curl->simple_post($config['c2dm']['google_send_url'], $data);
 
         if ($returned_data === FALSE)
         {
-            //var_dump($curl->errno);
-            //var_dump($curl->errmsg);
-            if($curl->http_code == 401)
+            if($curl->http_code == 401) //Auth token invalid
             {
-                echo "Auth token invalid";
-                //$authToken = $this->get_auth_token();
+                $authToken = $this->get_auth_token($config['c2dm']['username'], $config['c2dm']['password']);
+                return FALSE;
             }
             if($curl->http_code == 503)
             {
                 echo "Server temporarily unavailable";
+                return $curl->http_code;
             }
             else
             {
                 echo "FAILED posting to $url\n\n";
+                unset($curl);
             }
-
-
-            unset($curl);
 
             return FALSE;
         }
@@ -142,6 +152,13 @@ class C2DM
             unset($curl);
             return $returned_data;
         }
+    }
+
+    public function recover_auth_token()
+    {
+        $content = implode('', file(dirname(__FILE__) . "/../../../token_string.txt"));
+
+        return $content;
     }
 
 }
