832b0ab379faddb363b799f6cfadd5a039c8702b
Heinz Wiesinger <heinz@m2mobi.com>
2011-10-24 10:39:04 +0200

    tests: Implement test for prepare_data() for the case of keys

--- tests/system/libraries/db/class.dbcon.test.php
+++ tests/system/libraries/db/class.dbcon.test.php
@@ -27,9 +27,41 @@ class DBConReadonlyTest extends PHPUnit_Framework_TestCase
         $this->dbcon = new MockDBConReadonly();
     }
 
-    public function testpreparedatakeys()
+    /**
+     * Tests the escaping and preparation of key names for SQL queries
+     * @dataProvider datasetProvider
+     * @covers Lunr\Libraries\Database\DBCon::prepare_data
+     */
+    public function testPrepareDataKeys($data, $result)
     {
+        $method = $this->dbcon_reflection->getMethod('prepare_data');
+        $method->setAccessible(TRUE);
+        $this->assertEquals($result, $method->invokeArgs($this->dbcon, array($data, 'keys')));
+    }
 
+    public function datasetProvider()
+    {
+        $data[] = array(
+            array(
+                "key" => "value"
+            ),
+            "(`key`) "
+        );
+
+        $data[] = array(
+            array(
+                "key1" => "value1",
+                "key2" => "value2"
+            ),
+            "(`key1`,`key2`) "
+        );
+
+        $data[] = array(
+            "value1",
+            ""
+        );
+
+        return $data;
     }
 
 }
