5f8ad9f601dd8cf4642fe6fb76582e4428e65749
Heinz Wiesinger <heinz@m2mobi.com>
2011-12-28 16:15:51 +0100

    Logger: Add error logging class.
    Second part of replacing the Output class

---
+++ system/libraries/core/class.logger.inc.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * Error/Notice logging class.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    Core
+ * @subpackage Libraries
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+
+namespace Lunr\Libraries\Core;
+
+/**
+ * Class for logging errors or notices.
+ *
+ * @category   Libraries
+ * @package    Core
+ * @subpackage Libraries
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+class Logger
+{
+
+    /**
+     * Instance of the DateTime class.
+     * @var DateTime
+     */
+    private $datetime;
+
+    /**
+     * Reference to the Request class.
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * Constructor.
+     *
+     * @param DateTime $datetime Instance of the DateTime class.
+     */
+    public function __construct($datetime, &$request)
+    {
+        $this->datetime = $datetime;
+        $this->datetime->set_datetime_format('%Y-%m-%d %H:%M:%S');
+
+        $this->request =& $request;
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        unset($this->datetime);
+        $this->request = NULL;
+    }
+
+    /**
+     * Trigger a PHP warning.
+     *
+     * @param String $info The error string that should be printed
+     * @param String $file The log file the error should be logged to (optional)
+     *
+     * @return void
+     */
+    public function log_error($info, $file = '')
+    {
+        $prefix = '[' . $this->datetime->get_datetime() . ']: ';
+
+        if (($this->request->controller != NULL) && ($this->request->method != NULL))
+        {
+            $infix = $this->request->controller . '/' . $this->request->method . ': ';
+        }
+        else
+        {
+            $infix = '';
+        }
+
+        if ($file == '')
+        {
+            trigger_error($infix . $info, E_USER_WARNING);
+        }
+        else
+        {
+            if (function_exists('xdebug_print_function_stack') === TRUE)
+            {
+                xdebug_print_function_stack($infix . $info);
+            }
+            error_log($prefix . $infix . $info, 3, $file);
+        }
+    }
+
+    /**
+     * Trigger a PHP warning (with linebreak at the end).
+     *
+     * @param String $info The error string that should be printed
+     * @param String $file The log file the error should be logged to (optional)
+     *
+     * @return void
+     */
+    public function log_errorln($info, $file = '')
+    {
+        $this->log_error($info . "\n", $file);
+    }
+
+}
+
+?>
