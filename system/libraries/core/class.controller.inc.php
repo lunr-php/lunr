337dc78ff6b627db8930452e078df655d89450ad
Heinz Wiesinger <heinz@m2mobi.com>
2012-03-03 14:08:38 +0100

    controller: (Re-)Add abstract controller class
    With the introduction of dependency injection, having three different
    controller classes is no longer necessary. They are all doing the same
    now so go with a general purpose one.

---
+++ system/libraries/core/class.controller.inc.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * This file contains an abstract controller class.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    Core
+ * @subpackage Libraries
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+
+namespace Lunr\Libraries\Core;
+
+/**
+ * Controller class
+ *
+ * @category   Libraries
+ * @package    Core
+ * @subpackage Libraries
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+abstract class Controller
+{
+
+    /**
+     * Reference to the Response class.
+     * @var Response
+     */
+    protected $response;
+
+    /**
+     * Set of error enums.
+     * @var array
+     */
+    protected $error;
+
+    /**
+     * Constructor.
+     */
+    public function __construct(&$response)
+    {
+        $this->response =& $response;
+        $this->error = array();
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        unset($this->error);
+        $this->response = NULL;
+    }
+
+    /**
+     * Handle unimplemented calls.
+     *
+     * @param String $name      Method name
+     * @param array  $arguments Arguments passed to the method
+     *
+     * @return void
+     */
+    public function __call($name, $arguments)
+    {
+        if (isset($this->error['not_implemented']))
+        {
+            $this->response->errno = $this->error['not_implemented'];
+        }
+
+        $this->response->errmsg = "Not implemented!";
+    }
+
+    /**
+     * Store a set of error enums.
+     *
+     * @param array &$enums An array of error enums
+     *
+     * @return void
+     */
+    public function set_error_enums($enums)
+    {
+        $this->error =& $enums;
+    }
+
+}
+
+?>
