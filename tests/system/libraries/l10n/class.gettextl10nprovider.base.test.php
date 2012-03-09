7d8f3b506095cb8e7b4d52d0d0a8b6f7d4eaf7db
Heinz Wiesinger <heinz@m2mobi.com>
2012-03-09 15:47:30 +0100

    tests: Add base tests for the GettextL10nProvider class

---
+++ tests/system/libraries/l10n/class.gettextl10nprovider.base.test.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file contains the GettextL10nProviderBaseTest class.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    L10n
+ * @subpackage Tests
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+
+namespace Lunr\Libraries\L10n;
+
+/**
+ * This class contains the tests for the contructor and init function.
+ *
+ * @category   Libraries
+ * @package    L10n
+ * @subpackage Tests
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @covers     Lunr\Libraries\L10n\GettextL10nProvider
+ */
+class GettextL10nProviderBaseTest extends GettextL10nProviderTest
+{
+
+    /**
+     * Test that init() works correctly.
+     *
+     * @runInSeparateProcess
+     *
+     * @covers Lunr\Libraries\L10n\GettextL10nProvider::init
+     */
+    public function test_init()
+    {
+        $method = $this->provider_reflection->getMethod('init');
+        $method->setAccessible(TRUE);
+
+        $method->invokeArgs($this->provider, array(self::LANGUAGE));
+
+        $this->assertEquals(self::LANGUAGE, setlocale(LC_MESSAGES, 0));
+        $this->assertEquals($this->configuration['l10n']['domain'], textdomain(NULL));
+    }
+
+}
+
+?>
---
+++ tests/system/libraries/l10n/class.gettextl10nprovider.test.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * This file contains the GettextL10nProviderTest class.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    L10n
+ * @subpackage Tests
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+
+namespace Lunr\Libraries\L10n;
+use Lunr\Libraries\Core\DateTime;
+use PHPUnit_Framework_TestCase;
+use ReflectionClass;
+
+/**
+ * This class contains common setup routines, providers
+ * and shared attributes for testing the GettextL10nProvider class.
+ *
+ * @category   Libraries
+ * @package    L10n
+ * @subpackage Tests
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @covers     Lunr\Libraries\L10n\GettextL10nProvider
+ */
+abstract class GettextL10nProviderTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Instance of the GettextL10nProvider class.
+     * @var GettextL10nProvider
+     */
+    protected $provider;
+
+    /**
+     * Reflection instance of the GettextL10nProvider class.
+     * @var ReflectionClass
+     */
+    protected $provider_reflection;
+
+    /**
+     * Mock Object for the Configuration class.
+     * @var Configuration
+     */
+    protected $configuration;
+
+    /**
+     * The language used for testing.
+     * @var String
+     */
+    const LANGUAGE = "de_DE";
+
+    /**
+     * TestCase Constructor.
+     */
+    public function setUp()
+    {
+        $sub_configuration = $this->getMock('Lunr\Libraries\Core\Configuration');
+
+        $map = array(
+            array('domain', 'Lunr'),
+            array('locales', dirname(__FILE__) . "/../../../statics/l10n"),
+            array('default_language', 'nl_NL'),
+        );
+
+        $sub_configuration->expects($this->any())
+                          ->method('offsetGet')
+                          ->will($this->returnValueMap($map));
+
+        $this->configuration = $this->getMock('Lunr\Libraries\Core\Configuration');
+
+        $map = array(
+            array('l10n', $sub_configuration),
+        );
+
+        $this->configuration->expects($this->any())
+                      ->method('offsetGet')
+                      ->will($this->returnValueMap($map));
+
+        $logger = $this->getMockBuilder('Lunr\Libraries\Core\Logger')
+                       ->disableOriginalConstructor()
+                       ->getMock();
+
+        $this->provider = new GettextL10nProvider(self::LANGUAGE, $this->configuration, $logger);
+
+        $this->provider_reflection = new ReflectionClass('Lunr\Libraries\L10n\GettextL10nProvider');
+    }
+
+    /**
+     * TestCase Destructor.
+     */
+    public function tearDown()
+    {
+        unset($this->provider);
+        unset($this->provider_reflection);
+        unset($this->configuration);
+    }
+
+}
+
+?>
