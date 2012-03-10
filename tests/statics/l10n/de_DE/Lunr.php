3bb608135675e3e0aa8666c388a41ca546a9eeef
Heinz Wiesinger <heinz@m2mobi.com>
2012-03-10 19:20:02 +0100

    tests: Updated l10n test data

--- tests/statics/l10n/de_DE/Lunr.php
+++ tests/statics/l10n/de_DE/Lunr.php
@@ -7,6 +7,7 @@ $lang['bank']['finance'] = 'Bank';
 $lang['%d man']['%d men'][0] = "%d Mann";
 $lang['%d man']['%d men'][1] = "%d Männer";
 
+$lang['%d egg']['food'] = '%d Ei';
 $lang['%d egg']['%d eggs']['food'][0] = "%d Ei";
 $lang['%d egg']['%d eggs']['food'][1] = "%d Eier";
 
