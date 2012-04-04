7ae4c4e4f0f682e395ceca1c3ca73aad20c72f3f
Heinz Wiesinger <heinz@m2mobi.com>
2012-04-04 20:05:25 +0200

    datetime: cosmetics for the is_leap_year method

--- system/libraries/core/class.datetime.inc.php
+++ system/libraries/core/class.datetime.inc.php
@@ -268,7 +268,7 @@ class DateTime
      */
     public function is_leap_year($value)
     {
-        return ((($value % 4) == 0) && ((($value % 100) != 0) || (($value %400) == 0)));
+        return (($value % 4) == 0) && ((($value % 100) != 0) || (($value % 400) == 0));
     }
 
     /**
