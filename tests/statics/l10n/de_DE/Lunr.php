3651526741f23b9491a69c2bce7982cce8aa2573
Heinz Wiesinger <heinz@m2mobi.com>
2012-04-15 15:11:26 +0200

    tests: Codestyle fixes in the l10n static files

--- tests/statics/l10n/de_DE/Lunr.php
+++ tests/statics/l10n/de_DE/Lunr.php
@@ -4,11 +4,11 @@ $lang['table'] = 'Tisch';
 
 $lang['bank']['finance'] = 'Bank';
 
-$lang['%d man']['%d men'][0] = "%d Mann";
-$lang['%d man']['%d men'][1] = "%d Männer";
+$lang['%d man']['%d men'][0] = '%d Mann';
+$lang['%d man']['%d men'][1] = '%d Männer';
 
 $lang['%d egg']['food'] = '%d Ei';
-$lang['%d egg']['%d eggs']['food'][0] = "%d Ei";
-$lang['%d egg']['%d eggs']['food'][1] = "%d Eier";
+$lang['%d egg']['%d eggs']['food'][0] = '%d Ei';
+$lang['%d egg']['%d eggs']['food'][1] = '%d Eier';
 
 ?>
