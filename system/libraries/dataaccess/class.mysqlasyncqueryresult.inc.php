89b4ee1c8f192a007535b2869ec9389ae423ffe6
Heinz Wiesinger <heinz@m2mobi.com>
2012-04-18 11:39:50 +0200

    dataaccess: Add query result interface and classes.
    This comes with implementations for MySQL/MariaDB. One class for
    your standard query result, as it existed in 0.1 and another one
    for the result of async queries.

---
+++ system/libraries/dataaccess/class.mysqlasyncqueryresult.inc.php
@@ -0,0 +1,176 @@
+<?php
+
+/**
+ * MySQL asynchronous query result class.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    DataAccess
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @author     M2Mobi <info@m2mobi.com>
+ */
+
+namespace Lunr\Libraries\DataAccess;
+
+/**
+ * MySQL/MariaDB asynchronous query result class.
+ *
+ * @category   Libraries
+ * @package    DataAccess
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @author     M2Mobi <info@m2mobi.com>
+ */
+class MySQLAsyncQueryResult extends MySQLQueryResult
+{
+
+    /**
+     * Flag whether the result was fetched or not.
+     * @var Boolean
+     */
+    protected $fetched;
+
+    /**
+     * Constructor.
+     *
+     * @param MySQLi &$mysqli Reference to the MySQLi class
+     */
+    public function __construct(&$mysqli)
+    {
+        parent::__construct(FALSE, $mysqli);
+        $this->fetched =  FALSE;
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        unset($this->fetched);
+        parent::__destruct();
+    }
+
+    /**
+     * Retrieve results for an asynchronous query.
+     *
+     * @return void
+     */
+    protected function fetch_result()
+    {
+        if ($this->fetched === TRUE)
+        {
+            return;
+        }
+
+        $this->result = $this->mysqli->reap_async_query();
+
+        if ($this->result instanceof MySQLi_Result)
+        {
+            $this->success = TRUE;
+        }
+        else
+        {
+            $this->success = $this->result;
+        }
+
+        $this->fetched = TRUE;
+    }
+
+    /**
+     * Check whether the query has failed or not.
+     *
+     * @return Boolean $return TRUE if it failed, FALSE otherwise
+     */
+    public function has_failed()
+    {
+        $this->fetch_result();
+        return parent::has_failed();
+    }
+
+    /**
+     * Returns the number of rows affected by the last query.
+     *
+     * @return mixed $number Number of rows in the result set.
+     *                       This is usually an integer, unless the number is > MAXINT.
+     *                       Then it is a string.
+     */
+    public function affected_rows()
+    {
+        $this->fetch_result();
+        return parent::affected_rows();
+    }
+
+    /**
+     * Returns the number of rows in the result set.
+     *
+     * @return mixed $number Number of rows in the result set.
+     *                       This is usually an integer, unless the number is > MAXINT.
+     *                       Then it is a string.
+     */
+    public function number_of_rows()
+    {
+        $this->fetch_result();
+        return parent::number_of_rows();
+    }
+
+    /**
+     * Get the entire result set as an array.
+     *
+     * @return array $output Result set as array
+     */
+    public function result_array()
+    {
+        $this->fetch_result();
+        $output = parent::result_array();
+        $this->result->free();
+        return $output;
+    }
+
+    /**
+     * Get the first row of the result set.
+     *
+     * @return array $output First result row as array
+     */
+    public function result_row()
+    {
+        $this->fetch_result();
+        $output = parent::result_row();
+        $this->result->free();
+        return $output;
+    }
+
+    /**
+     * Get a specific column of the result set.
+     *
+     * @param String $column Column or Alias name
+     *
+     * @return array $output Result column as array
+     */
+    public function result_column($column)
+    {
+        $this->fetch_result();
+        $output = parent::result_column($column);
+        $this->result->free();
+        return $output;
+    }
+
+    /**
+     * Get a specific column of the first row of the result set.
+     *
+     * @param String $column Column or Alias name
+     *
+     * @return mixed $output NULL if it does not exist, the value otherwise
+     */
+    public function result_cell($column)
+    {
+        $this->fetch_result();
+        $output = parent::result_cell($column);
+        $this->result->free();
+        return $output;
+    }
+
+}
+
+?>
---
+++ system/libraries/dataaccess/class.mysqlqueryresult.inc.php
@@ -0,0 +1,196 @@
+<?php
+
+/**
+ * MySQL query result class.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    DataAccess
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @author     M2Mobi <info@m2mobi.com>
+ */
+
+namespace Lunr\Libraries\DataAccess;
+
+/**
+ * MySQL/MariaDB query result class.
+ *
+ * @category   Libraries
+ * @package    DataAccess
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @author     M2Mobi <info@m2mobi.com>
+ */
+class MySQLQueryResult implements DatabaseQueryResultInterface
+{
+
+    /**
+     * Return value from mysqli->query().
+     * @var mixed
+     */
+    protected $result;
+
+    /**
+     * Reference to the mysqli class.
+     * @var MySQLi
+     */
+    protected $mysqli;
+
+    /**
+     * Flag whether the query was successful or not.
+     * @var Boolean
+     */
+    protected $success;
+
+    /**
+     * Constructor.
+     *
+     * @param mixed  $result  Query result
+     * @param MySQLi &$mysqli Reference to the MySQLi class
+     */
+    public function __construct($result, &$mysqli)
+    {
+        if ($result instanceof MySQLi_Result)
+        {
+            $this->success = TRUE;
+        }
+        else
+        {
+            $this->success = $result;
+        }
+
+        $this->result =  $result;
+        $this->mysqli =& $mysqli;
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        $this->mysqli = NULL;
+
+        unset($this->result);
+        unset($this->success);
+    }
+
+    /**
+     * Check whether the query has failed or not.
+     *
+     * @return Boolean $return TRUE if it failed, FALSE otherwise
+     */
+    public function has_failed()
+    {
+        return !$this->success;
+    }
+
+    /**
+     * Returns the number of rows affected by the last query.
+     *
+     * @return mixed $number Number of rows in the result set.
+     *                       This is usually an integer, unless the number is > MAXINT.
+     *                       Then it is a string.
+     */
+    public function affected_rows()
+    {
+        return $this->mysqli->affected_rows;
+    }
+
+    /**
+     * Returns the number of rows in the result set.
+     *
+     * @return mixed $number Number of rows in the result set.
+     *                       This is usually an integer, unless the number is > MAXINT.
+     *                       Then it is a string.
+     */
+    public function number_of_rows()
+    {
+        if (is_bool($this->result))
+        {
+            return $this->mysqli->affected_rows;
+        }
+        else
+        {
+            return $this->result->num_rows;
+        }
+    }
+
+    /**
+     * Get the entire result set as an array.
+     *
+     * @return array $output Result set as array
+     */
+    public function result_array()
+    {
+        $output = array();
+
+        if (is_bool($this->result))
+        {
+           return $output;
+        }
+
+        while ($row = $this->result->fetch_assoc())
+        {
+            $output[] = $row;
+        }
+
+        return $output;
+    }
+
+    /**
+     * Get the first row of the result set.
+     *
+     * @return array $output First result row as array
+     */
+    public function result_row()
+    {
+        return is_bool($this->result) ? array() : $this->result->fetch_assoc();
+    }
+
+    /**
+     * Get a specific column of the result set.
+     *
+     * @param String $column Column or Alias name
+     *
+     * @return array $output Result column as array
+     */
+    public function result_column($column)
+    {
+        $output = array();
+
+        if (is_bool($this->result))
+        {
+           return $output;
+        }
+
+        while ($row = $this->result->fetch_assoc())
+        {
+            $output[] = $row[$column];
+        }
+
+        return $output;
+    }
+
+    /**
+     * Get a specific column of the first row of the result set.
+     *
+     * @param String $column Column or Alias name
+     *
+     * @return mixed $output NULL if it does not exist, the value otherwise
+     */
+    public function result_cell($column)
+    {
+        if (is_bool($this->result))
+        {
+            return NULL;
+        }
+
+        $line = $this->result->fetch_assoc();
+        return isset($line[$column]) ? $line[$column] : NULL;
+    }
+
+}
+
+?>
