08ebfe92e4d86419d169d9d453c824753d6f87d4
Heinz Wiesinger <heinz@m2mobi.com>
2012-05-03 15:00:02 +0200

    tests: Add unit tests for running queries on a mysql connection

---
+++ tests/system/libraries/dataaccess/class.mysqlconnection.query.test.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * This file contains the MySQLConnectionQueryTest class.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    DataAccess
+ * @subpackage Tests
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+
+namespace Lunr\Libraries\DataAccess;
+
+/**
+ * This class contains query related unit tests for MySQLConnection.
+ *
+ * @category   Libraries
+ * @package    DataAccess
+ * @subpackage Tests
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @covers     Lunr\Libraries\DataAccess\MySQLConnection
+ */
+class MySQLConnectionQueryTest extends MySQLConnectionTest
+{
+
+    /**
+     * Test that query() returns a QueryResult that indicates a failed query when not connected.
+     *
+     * @covers Lunr\Libraries\DataAccess\MySQLConnection::query
+     */
+    public function testQueryReturnsFailedQueryResultWhenNotConnected()
+    {
+        $mysqli = new \mysqli();
+
+        $class = $this->db_reflection->getProperty('mysqli');
+        $class->setAccessible(TRUE);
+        $class->setValue($this->db, $mysqli);
+
+        mysqlnd_uh_set_connection_proxy(new MockMySQLndFailedConnection());
+
+        $query = $this->db->query('query');
+
+        $this->assertInstanceOf('Lunr\Libraries\DataAccess\MySQLQueryResult', $query);
+        $this->assertTrue($query->has_failed());
+    }
+
+    /**
+     * Test that query() returns a QueryResult when connected.
+     *
+     * @covers Lunr\Libraries\DataAccess\MySQLConnection::query
+     */
+    public function testQueryReturnsQueryResultWhenConnected()
+    {
+        $property = $this->db_reflection->getProperty('connected');
+        $property->setAccessible(TRUE);
+        $property->setValue($this->db, TRUE);
+
+        $this->mysqli->expects($this->once())
+                     ->method('query')
+                     ->will($this->returnValue(TRUE));
+
+        $query = $this->db->query('query');
+
+        $this->assertInstanceOf('Lunr\Libraries\DataAccess\MySQLQueryResult', $query);
+        $this->assertFalse($query->has_failed());
+
+        $property->setValue($this->db, FALSE);
+    }
+
+    /**
+     * Test that async_query() returns an AsyncQueryResult that indicates a failed query when not connected.
+     *
+     * @covers Lunr\Libraries\DataAccess\MySQLConnection::async_query
+     */
+    public function testAsyncQueryReturnsFailedQueryResultWhenNotConnected()
+    {
+        $mysqli = new \mysqli();
+
+        $class = $this->db_reflection->getProperty('mysqli');
+        $class->setAccessible(TRUE);
+        $class->setValue($this->db, $mysqli);
+
+        mysqlnd_uh_set_connection_proxy(new MockMySQLndFailedConnection());
+
+        $query = $this->db->async_query('query');
+
+        $this->assertInstanceOf('Lunr\Libraries\DataAccess\MySQLQueryResult', $query);
+        $this->assertTrue($query->has_failed());
+    }
+
+    /**
+     * Test that async_query() returns a AsyncQueryResult when connected.
+     *
+     * @covers Lunr\Libraries\DataAccess\MySQLConnection::async_query
+     */
+    public function testAsyncQueryReturnsQueryResultWhenConnected()
+    {
+        $property = $this->db_reflection->getProperty('connected');
+        $property->setAccessible(TRUE);
+        $property->setValue($this->db, TRUE);
+
+        $this->mysqli->expects($this->once())
+                     ->method('query');
+
+        $this->mysqli->expects($this->once())
+                     ->method('reap_async_query')
+                     ->will($this->returnValue(TRUE));
+
+        $query = $this->db->async_query('query');
+
+        $this->assertInstanceOf('Lunr\Libraries\DataAccess\MySQLAsyncQueryResult', $query);
+        $this->assertFalse($query->has_failed());
+
+        $property->setValue($this->db, FALSE);
+    }
+
+}
+
+?>
