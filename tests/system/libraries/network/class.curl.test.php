e617792fb0ba0c69338eb7f5c345893c343a34eb
Heinz Wiesinger <heinz@m2mobi.com>
2012-05-11 14:51:48 +0200

    tests: Add unit tests for the getters and setters of the curl class

--- tests/system/libraries/network/class.curl.test.php
+++ tests/system/libraries/network/class.curl.test.php
@@ -58,6 +58,39 @@ abstract class CurlTest extends PHPUnit_Framework_TestCase
         unset($this->curl);
     }
 
+    /**
+     * Unit Test Data Provider for valid property values.
+     *
+     * @return array $properties Array of valid properties of the Curl class
+     */
+    public function validPropertyProvider()
+    {
+        $properties   = array();
+        $properties[] = array('errno', 0);
+        $properties[] = array('errmsg', '');
+        $properties[] = array('info', array());
+        $properties[] = array('http_code', 0);
+
+        return $properties;
+    }
+
+    /**
+     * Unit Test Data Provider for invalid Curl options.
+     *
+     * @return array $options Array of invalid Curl options.
+     */
+    public function invalidOptionProvider()
+    {
+        $options   = array();
+        $options[] = array('RANDOM_OPTION');
+        $options[] = array('C_OPT');
+        $options[] = array('CU_OPT');
+        $options[] = array('CUR_OPT');
+        $options[] = array('curl_OPT');
+
+        return $options;
+    }
+
 }
 
 ?>
