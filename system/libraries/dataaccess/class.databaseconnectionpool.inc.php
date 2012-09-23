bc41ffc9471106ba4a166185eb31b9628ee0a045
Heinz Wiesinger <heinz@m2mobi.com>
2012-09-23 16:05:06 +0200

    dataaccess: Add database connection pooling class.

---
+++ system/libraries/dataaccess/class.databaseconnectionpool.inc.php
@@ -0,0 +1,164 @@
+<?php
+
+/**
+ * Database connection pool class.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    DataAccess
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @author     M2Mobi <info@m2mobi.com>
+ */
+
+namespace Lunr\Libraries\DataAccess;
+
+/**
+ * This class implements a simple database connection pool.
+ *
+ * @category   Libraries
+ * @package    DataAccess
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @author     M2Mobi <info@m2mobi.com>
+ */
+class DatabaseConnectionPool
+{
+
+    /**
+     * Reference to the Configuration class
+     * @var Configuration
+     */
+    protected $configuration;
+
+    /**
+     * Reference to the Logger class
+     * @var Logger
+     */
+    protected $logger;
+
+    /**
+     * Readonly connection pool
+     * @var array
+     */
+    protected $ro_pool;
+
+    /**
+     * Read-Write connection pool
+     * @var array
+     */
+    protected $rw_pool;
+
+    /**
+     * Constructor.
+     *
+     * @param Configuration &$configuration Reference to the configuration class
+     * @param Logger        &$logger        Reference to the logger class
+     */
+    public function __construct(&$configuration, &$logger)
+    {
+        $this->configuration =& $configuration;
+        $this->logger        =& $logger;
+
+        $this->ro_pool = array();
+        $this->rw_pool = array();
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        $this->configuration = NULL;
+        $this->logger        = NULL;
+
+        unset($this->ro_pool);
+        unset($this->rw_pool);
+    }
+
+    /**
+     * Get a new readonly connection from the pool.
+     *
+     * @return DatabaseConnection $db A database connection
+     */
+    public function get_new_ro_connection()
+    {
+        return $this->get_connection(TRUE, TRUE);
+    }
+
+    /**
+     * Get a new read-write connection from the pool.
+     *
+     * @return DatabaseConnection $db A database connection
+     */
+    public function get_new_rw_connection()
+    {
+        return $this->get_connection(TRUE, FALSE);
+    }
+
+    /**
+     * Get an existing readonly connection from the pool.
+     *
+     * @return DatabaseConnection $db A database connection
+     */
+    public function get_ro_connection()
+    {
+        return $this->get_connection(FALSE, TRUE);
+    }
+
+    /**
+     * Get an existing read-write connection from the pool.
+     *
+     * @return DatabaseConnection $db A database connection
+     */
+    public function get_rw_connection()
+    {
+        return $this->get_connection(FALSE, FALSE);
+    }
+
+    /**
+     * Get a database connection.
+     *
+     * @param Boolean $new Whether to get a new connection or not
+     * @param Boolean $ro  Whether to get a readonly connection or not
+     *
+     * @return DatabaseConnection $db A database connection
+     */
+    protected function get_connection($new, $ro)
+    {
+        $store = $ro ? 'ro_pool' : 'rw_pool';
+
+        switch ($this->configuration['db']['driver'])
+        {
+            case 'mysql':
+                $type  = 'MySQLConnection';
+                $extra = new MySQLi();
+                break;
+            default:
+                return NULL;
+                break;
+        }
+
+        if ($new === TRUE)
+        {
+            $connection = new $type($this->configuration, $this->logger, $extra);
+
+            if ($ro === TRUE)
+            {
+                $connection->set_readonly(TRUE);
+            }
+
+            $this->$store[] =& $connection;
+        }
+        else
+        {
+            $connection =& $this->$store[0];
+        }
+
+        return $connection;
+    }
+
+}
+
+?>
