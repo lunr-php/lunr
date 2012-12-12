3b53cf436abaaf7a7b18730d0029687d170a1f47
Olivier Wizen <olivier@m2mobi.com>
2012-12-12 16:38:24 +0100

    spwn: Add job and dispatcher interfaces.

---
+++ system/libraries/spwn/interface.backgroundjob.inc.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file contains an abstract definition for a background job class.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    Spwn
+ * @subpackage Libraries
+ * @author     Olivier Wizen <olivier@m2mobi.com>
+ * @copyright  2012, M2Mobi BV, Amsterdam, The Netherlands
+ * @license    http://lunr.nl/LICENSE MIT License
+ */
+
+namespace Lunr\Libraries\Spwn;
+
+/**
+ * Abstract definition for a a background job .
+ *
+ * @category   Libraries
+ * @package    Spwn
+ * @subpackage Libraries
+ * @author     Olivier Wizen <olivier@m2mobi.com>
+ */
+interface BackgroundJobInterface
+{
+
+    /**
+     * Sets up the environment for this job.
+     *
+     * @return void
+     */
+    public function setUp();
+
+    /**
+     * Performs the background job.
+     *
+     * @return void
+     */
+    public function perfom();
+
+    /**
+     * Tears down the environment for this job.
+     *
+     * @return void
+     */
+    public function tearDown();
+
+}
+
+?>
