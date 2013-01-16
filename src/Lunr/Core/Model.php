2e3f706b7f848b5ab5868176a9f487bbb09d13fd
Felipe Martinez <felipe@m2mobi.com>
2013-01-16 11:44:34 +0100

    Core: Adapted model class to 2.0

--- src/Lunr/Core/Model.php
+++ src/Lunr/Core/Model.php
@@ -1,13 +1,14 @@
 <?php
 
 /**
- * This file contains the base Model class.
+ * This file contains the Model interface.
  *
  * PHP Version 5.3
  *
  * @category   Libraries
  * @package    Core
  * @subpackage Libraries
+ * @author     Felipe Martinez <felipe@m2mobi.com>
  * @author     Heinz Wiesinger <heinz@m2mobi.com>
  * @copyright  2010-2013, M2Mobi BV, Amsterdam, The Netherlands
  * @license    http://lunr.nl/LICENSE MIT License
@@ -15,70 +16,17 @@
 
 namespace Lunr\Core;
 
-use Lunr\Database\DBMan;
-
 /**
  * Base Model class
  *
  * @category   Libraries
  * @package    Core
  * @subpackage Libraries
+ * @author     Felipe Martinez <felipe@m2mobi.com>
  * @author     Heinz Wiesinger <heinz@m2mobi.com>
  */
-abstract class Model
+interface Model
 {
-
-    /**
-     * Instance of the database connection
-     * @var DBCon
-     */
-    protected $db;
-
-    /**
-     * The db configuration given on construction
-     * @var array
-     */
-    protected $db_config;
-
-    /**
-     * Constructor.
-     *
-     * @param array $db The database configuration parameters
-     */
-    public function __construct($db)
-    {
-        $this->db        = DBMan::get_db_connection($db);
-        $this->db_config = $db;
-    }
-
-    /**
-     * Destructor.
-     */
-    public function __destruct()
-    {
-        unset($this->db);
-    }
-
-    /**
-     * Get write access for the database.
-     *
-     * @return Boolean $return TRUE if successful, FALSE if there's already a
-     *                         connection established
-     */
-    public function get_write_access()
-    {
-        $this->db = DBMan::get_db_connection($this->db_config, FALSE);
-    }
-
-    /**
-     * Disconnect from the database.
-     *
-     * @return void
-     */
-    public function disconnect()
-    {
-        $this->db->disconnect();
-    }
 
 }
 
