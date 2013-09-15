976acfc0a9111d3b2c96504fe0a47f9cc6a3e941
Heinz Wiesinger <heinz@m2mobi.com>
2013-09-15 16:57:38 +0200

    L10n: Port L10n class unit tests to LunrBaseTest.

---
+++ src/Lunr/L10n/Tests/L10nBaseTest.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ * This file contains the L10nTest class.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Libraries
+ * @package    L10n
+ * @subpackage Tests
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @copyright  2012-2013, M2Mobi BV, Amsterdam, The Netherlands
+ * @license    http://lunr.nl/LICENSE MIT License
+ */
+
+namespace Lunr\L10n\Tests;
+
+use Lunr\L10n\L10n;
+use PHPUnit_Framework_TestCase;
+use ReflectionClass;
+
+/**
+ * This class contains test methods for the L10n class.
+ *
+ * @category   Libraries
+ * @package    L10n
+ * @subpackage Tests
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @covers     Lunr\L10n\L10n
+ */
+class L10nBaseTest extends L10nTest
+{
+
+    /**
+     * Test that $languages is initially empty.
+     */
+    public function testLanguagesEmpty()
+    {
+        $properties = $this->reflection->getStaticProperties();
+        $languages  = $properties['languages'];
+
+        $this->assertEmpty($languages);
+    }
+
+    /**
+     * Test that the Logger class is passed correctly.
+     */
+    public function testLoggerIsPassedCorrectly()
+    {
+        $this->assertPropertySame('logger', $this->logger);
+    }
+
+    /**
+     * Test that the FilesystemAccessObject class is passed correctly.
+     */
+    public function testFAOIsPassedCorrectly()
+    {
+        $this->assertPropertySame('fao', $this->fao);
+    }
+
+    /**
+     * Test that the language is correctly stored in the object.
+     */
+    public function testDefaultLanguageSetCorrectly()
+    {
+        $this->assertPropertyEquals('default_language', 'en_US');
+    }
+
+    /**
+     * Test that the language is correctly stored in the object.
+     */
+    public function testLocaleLocationSetCorrectly()
+    {
+        // /usr/bin/l10n by default
+        $default_location = dirname($_SERVER['PHP_SELF']) . '/l10n';
+
+        $this->assertPropertyEquals('locales_location', $default_location);
+    }
+
+    /**
+     * Test initial call to get_supported_languages().
+     *
+     * @depends testLanguagesEmpty
+     * @covers  Lunr\L10n\L10n::get_supported_languages
+     */
+    public function testInitialGetSupportedLanguages()
+    {
+        $this->fao->expects($this->once())
+                  ->method('get_list_of_directories')
+                  ->with(dirname($_SERVER['PHP_SELF']) . '/l10n')
+                  ->will($this->returnValue(array('de_DE', 'nl_NL')));
+
+        $languages = $this->class->get_supported_languages();
+        sort($languages);
+        $this->assertEquals($this->languages, $languages);
+    }
+
+    /**
+     * Test that get_supported_languages() populated $languages.
+     *
+     * @depends testInitialGetSupportedLanguages
+     */
+    public function testLanguagesPopulated()
+    {
+        $properties = $this->reflection->getStaticProperties();
+        $languages  = $properties['languages'];
+        sort($languages);
+        $this->assertEquals($this->languages, $languages);
+    }
+
+    /**
+     * Test get_supported_languages() when it was already executed before.
+     *
+     * @depends testLanguagesPopulated
+     * @covers  Lunr\L10n\L10n::get_supported_languages
+     */
+    public function testCachedGetSupportedLanguages()
+    {
+        $this->fao->expects($this->never())
+                  ->method('get_list_of_directories');
+
+        $languages = $this->class->get_supported_languages();
+        sort($languages);
+        $this->assertEquals($this->languages, $languages);
+    }
+
+    /**
+     * Test iso_to_posix() with supported languages.
+     *
+     * @param String $iso   ISO language definition
+     * @param String $posix POSIX language definition
+     *
+     * @depends      testCachedGetSupportedLanguages
+     * @dataProvider supportedLanguagesProvider
+     * @covers       Lunr\L10n\L10n::iso_to_posix
+     */
+    public function testIsoToPosixForSupportedLanguages($iso, $posix)
+    {
+        $this->assertEquals($posix, $this->class->iso_to_posix($iso));
+    }
+
+    /**
+     * Test iso_to_posix() with unsupported languages.
+     *
+     * @param String $iso ISO language definition
+     *
+     * @depends      testCachedGetSupportedLanguages
+     * @dataProvider unsupportedLanguagesProvider
+     * @covers       Lunr\L10n\L10n::iso_to_posix
+     */
+    public function testIsoToPosixForUnsupportedLanguages($iso)
+    {
+        $this->assertEquals('en_US', $this->class->iso_to_posix($iso));
+    }
+
+}
+
+?>
--- src/Lunr/L10n/Tests/L10nTest.php
+++ src/Lunr/L10n/Tests/L10nTest.php
@@ -3,7 +3,7 @@
 /**
  * This file contains the L10nTest class.
  *
- * PHP Version 5.3
+ * PHP Version 5.4
  *
  * @category   Libraries
  * @package    L10n
@@ -16,7 +16,7 @@
 namespace Lunr\L10n\Tests;
 
 use Lunr\L10n\L10n;
-use PHPUnit_Framework_TestCase;
+use Lunr\Halo\LunrBaseTest;
 use ReflectionClass;
 
 /**
@@ -28,19 +28,8 @@ use ReflectionClass;
  * @author     Heinz Wiesinger <heinz@m2mobi.com>
  * @covers     Lunr\L10n\L10n
  */
-class L10nTest extends PHPUnit_Framework_TestCase
+abstract class L10nTest extends LunrBaseTest
 {
-
-    /**
-     * Instance of the L10n class.
-     * @var L10n
-     */
-    protected $class;
-
-    /**
-     * Reflection Instance of the L10n class.
-     */
-    protected $reflection;
 
     /**
      * Mock instance of a Logger class.
@@ -72,7 +61,7 @@ class L10nTest extends PHPUnit_Framework_TestCase
 
         $this->reflection = new ReflectionClass('Lunr\L10n\L10n');
 
-        $this->languages = array('de_DE', 'en_US', 'nl_NL');
+        $this->languages = [ 'de_DE', 'en_US', 'nl_NL' ];
     }
 
     /**
@@ -88,154 +77,15 @@ class L10nTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test that $languages is initially empty.
-     */
-    public function testLanguagesEmpty()
-    {
-        $properties = $this->reflection->getStaticProperties();
-        $languages  = $properties['languages'];
-        $this->assertEmpty($languages);
-    }
-
-    /**
-     * Test that the Logger class is passed correctly.
-     */
-    public function testLoggerIsPassedCorrectly()
-    {
-        $property = $this->reflection->getProperty('logger');
-        $property->setAccessible(TRUE);
-
-        $value = $property->getValue($this->class);
-
-        $this->assertInstanceOf('Psr\Log\LoggerInterface', $value);
-        $this->assertSame($this->logger, $value);
-    }
-
-    /**
-     * Test that the FilesystemAccessObject class is passed correctly.
-     */
-    public function testFAOIsPassedCorrectly()
-    {
-        $property = $this->reflection->getProperty('fao');
-        $property->setAccessible(TRUE);
-
-        $value = $property->getValue($this->class);
-
-        $this->assertInstanceOf('Lunr\Gravity\Filesystem\FilesystemAccessObjectInterface', $value);
-        $this->assertSame($this->fao, $value);
-    }
-
-    /**
-     * Test that the language is correctly stored in the object.
-     */
-    public function testDefaultLanguageSetCorrectly()
-    {
-        $property = $this->reflection->getProperty('default_language');
-        $property->setAccessible(TRUE);
-
-        $this->assertEquals('en_US', $property->getValue($this->class));
-    }
-
-    /**
-     * Test that the language is correctly stored in the object.
-     */
-    public function testLocaleLocationSetCorrectly()
-    {
-        $property = $this->reflection->getProperty('locales_location');
-        $property->setAccessible(TRUE);
-
-        // /usr/bin/l10n by default
-        $default_location = dirname($_SERVER['PHP_SELF']) . '/l10n';
-
-        $this->assertEquals($default_location, $property->getValue($this->class));
-    }
-
-    /**
-     * Test initial call to get_supported_languages().
-     *
-     * @depends testLanguagesEmpty
-     * @covers  Lunr\L10n\L10n::get_supported_languages
-     */
-    public function testInitialGetSupportedLanguages()
-    {
-        $this->fao->expects($this->once())
-                  ->method('get_list_of_directories')
-                  ->with(dirname($_SERVER['PHP_SELF']) . '/l10n')
-                  ->will($this->returnValue(array('de_DE', 'nl_NL')));
-
-        $languages = $this->class->get_supported_languages();
-        sort($languages);
-        $this->assertEquals($this->languages, $languages);
-    }
-
-    /**
-     * Test that get_supported_languages() populated $languages.
-     *
-     * @depends testInitialGetSupportedLanguages
-     */
-    public function testLanguagesPopulated()
-    {
-        $properties = $this->reflection->getStaticProperties();
-        $languages  = $properties['languages'];
-        sort($languages);
-        $this->assertEquals($this->languages, $languages);
-    }
-
-    /**
-     * Test get_supported_languages() when it was already executed before.
-     *
-     * @depends testLanguagesPopulated
-     * @covers  Lunr\L10n\L10n::get_supported_languages
-     */
-    public function testCachedGetSupportedLanguages()
-    {
-        $this->fao->expects($this->never())
-                  ->method('get_list_of_directories');
-
-        $languages = $this->class->get_supported_languages();
-        sort($languages);
-        $this->assertEquals($this->languages, $languages);
-    }
-
-    /**
-     * Test iso_to_posix() with supported languages.
-     *
-     * @param String $iso   ISO language definition
-     * @param String $posix POSIX language definition
-     *
-     * @depends      testCachedGetSupportedLanguages
-     * @dataProvider supportedLanguagesProvider
-     * @covers       Lunr\L10n\L10n::iso_to_posix
-     */
-    public function testIsoToPosixForSupportedLanguages($iso, $posix)
-    {
-        $this->assertEquals($posix, $this->class->iso_to_posix($iso));
-    }
-
-    /**
-     * Test iso_to_posix() with unsupported languages.
-     *
-     * @param String $iso ISO language definition
-     *
-     * @depends      testCachedGetSupportedLanguages
-     * @dataProvider unsupportedLanguagesProvider
-     * @covers       Lunr\L10n\L10n::iso_to_posix
-     */
-    public function testIsoToPosixForUnsupportedLanguages($iso)
-    {
-        $this->assertEquals('en_US', $this->class->iso_to_posix($iso));
-    }
-
-    /**
      * Unit Test Data Provider for supported languages.
      *
      * @return array $languages Array of supported languages
      */
     public function supportedLanguagesProvider()
     {
-        $languages   = array();
-        $languages[] = array('en', 'en_US');
-        $languages[] = array('nl', 'nl_NL');
+        $languages   = [];
+        $languages[] = [ 'en', 'en_US' ];
+        $languages[] = [ 'nl', 'nl_NL' ];
 
         return $languages;
     }
@@ -247,8 +97,8 @@ class L10nTest extends PHPUnit_Framework_TestCase
      */
     public function unsupportedLanguagesProvider()
     {
-        $languages   = array();
-        $languages[] = array('fr', 'fr_FR');
+        $languages   = [];
+        $languages[] = [ 'fr', 'fr_FR' ];
 
         return $languages;
     }
