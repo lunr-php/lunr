ff57ed56973266730ef4cc11fcf6a5e74079db32
Heinz Wiesinger <heinz@m2mobi.com>
2013-10-30 22:33:54 +0100

    Halo: Add PsrLoggerTestTrait.
    This trait contains a test method that checks whether a logger was
    correctly stored as property after passing it to the constructor.

---
+++ src/Lunr/Halo/PsrLoggerTestTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file contains the PsrLoggerTestTrait.
+ *
+ * PHP Version 5.4
+ *
+ * @category   Tests
+ * @package    Halo
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @copyright  2013, M2Mobi BV, Amsterdam, The Netherlands
+ * @license    http://lunr.nl/LICENSE MIT License
+ */
+
+namespace Lunr\Halo;
+
+/**
+ * This trait contains test methods to verify a PSR-3 compliant logger was passed correctly.
+ *
+ * @category   Tests
+ * @package    Halo
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+trait PsrLoggerTestTrait
+{
+
+    /**
+     * Test that the Logger class is passed correctly.
+     */
+    public function testLoggerIsSetCorrectly()
+    {
+        $property = $this->get_reflection_property_value('logger');
+
+        $this->assertSame($property, $this->logger);
+        $this->assertInstanceOf('Psr\Log\LoggerInterface', $property);
+    }
+
+}
+
+?>
