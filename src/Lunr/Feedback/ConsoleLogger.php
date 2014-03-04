78dcefb3b796cc97e834929bed340510a9fce0ea
Heinz Wiesinger <heinz@m2mobi.com>
2014-03-04 11:03:35 +0100

    Feedback: Add ConsoleLogger class.
    This is a PSR-3 compatible logger that logs messages to the command
    line using the Console class.

---
+++ src/Lunr/Feedback/ConsoleLogger.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * A class for logging messages to the command line.
+ *
+ * PHP Version 5.4
+ *
+ * @category   Libraries
+ * @package    Feedback
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @copyright  2014, M2Mobi BV, Amsterdam, The Netherlands
+ * @license    http://lunr.nl/LICENSE MIT License
+ */
+
+namespace Lunr\Feedback;
+
+/**
+ * A class for logging messages to the command line.
+ *
+ * @category   Libraries
+ * @package    Feedback
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+class ConsoleLogger extends AbstractLogger
+{
+
+    /**
+     * Shared instance of the Console class.
+     * @var \Lunr\Shadow\Console
+     */
+    protected $console;
+
+    /**
+     * Constructor.
+     *
+     * @param \Lunr\Corona\RequestInterface $request  Shared instance of the Request class.
+     * @param \Lunr\Shadow\Console          $console  Shared instance of the Console class.
+     */
+    public function __construct($request, $console)
+    {
+        parent::__construct($request);
+
+        $this->console = $console;
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        unset($this->console);
+
+        parent::__destruct();
+    }
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed  $level   Log level (severity)
+     * @param String $message Log Message
+     * @param array  $context Additional meta-information for the log
+     *
+     * @return void
+     */
+    public function log($level, $message, array $context = array())
+    {
+        $message = strtoupper($level) . ': ' . (string) $message;
+        $msg     = $this->compose_message($message, $context);
+
+        $this->console->cli_println($msg);
+    }
+
+}
+
+?>
---
+++ src/Lunr/Feedback/Tests/ConsoleLoggerBaseTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * This file contains the ConsoleLoggerBaseTest class.
+ *
+ * PHP Version 5.4
+ *
+ * @category   Libraries
+ * @package    Feedback
+ * @subpackage Tests
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @copyright  2014, M2Mobi BV, Amsterdam, The Netherlands
+ * @license    http://lunr.nl/LICENSE MIT License
+ */
+
+namespace Lunr\Feedback\Tests;
+
+use Psr\Log\LogLevel;
+
+/**
+ * This class contains test methods for the ConsoleLogger class.
+ *
+ * @category   Libraries
+ * @package    Feedback
+ * @subpackage Tests
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @covers     Lunr\Feedback\ConsoleLogger
+ */
+class ConsoleLoggerBaseTest extends ConsoleLoggerTest
+{
+
+    /**
+     * Test that the request class is set correctly.
+     */
+    public function testRequestSetCorrectly()
+    {
+        $this->assertPropertySame('request', $this->request);
+    }
+
+    /**
+     * Test that the console class is set correctly.
+     */
+    public function testConsoleSetCorrectly()
+    {
+        $this->assertPropertySame('console', $this->console);
+    }
+
+    /**
+     * Test that log() logs correctly to the console.
+     *
+     * @covers Lunr\Feedback\ConsoleLogger::log
+     */
+    public function testLogOutputsMessageOnCommandLine()
+    {
+        $this->console->expects($this->once())
+                      ->method('cli_println')
+                      ->with($this->equalTo("WARNING: Foo"));
+
+        $this->class->log(LogLevel::WARNING, 'Foo');
+    }
+
+}
+
+?>
