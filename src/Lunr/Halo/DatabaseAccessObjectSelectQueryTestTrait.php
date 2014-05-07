37cf45a9b3f05588bd19f3358dfb85b6c0ad2458
Heinz Wiesinger <heinz@m2mobi.com>
2014-05-07 15:07:50 +0200

    Halo: Add DAO Test helper classes.
    This adds a base test class for setting up tests that use MySQL
    Database access. Additionally there is now a trait for shared
    expectations for generic SELECT query results.

---
+++ src/Lunr/Halo/DatabaseAccessObjectSelectQueryTestTrait.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * This file contains the DatabaseAccessObjectSelectQueryTestTrait.
+ *
+ * PHP Version 5.4
+ *
+ * @category   Tests
+ * @package    Halo
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @copyright  2014, M2Mobi BV, Amsterdam, The Netherlands
+ * @license    http://lunr.nl/LICENSE MIT License
+ */
+
+namespace Lunr\Halo;
+
+/**
+ * This trait contains helper methods to test general success and error cases of SELECT queries.
+ *
+ * @category   Tests
+ * @package    Halo
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+trait DatabaseAccessObjectSelectQueryTestTrait
+{
+
+    /**
+     * Expect that a query returns successful results.
+     *
+     * @param mixed  $data   Result data
+     * @param String $format Return result as 'array', 'row', 'column' or 'cell'
+     *
+     * @return void
+     */
+    public function expextResultOnSuccess($data, $format = 'array')
+    {
+        $mock = new FluidInterfaceMock();
+
+        $this->db->expects($this->once())
+                 ->method('get_new_dml_query_builder_object')
+                 ->will($this->returnValue($mock));
+
+        $this->db->expects($this->once())
+                 ->method('query')
+                 ->will($this->returnValue($this->result));
+
+        $this->result->expects($this->once())
+                     ->method('has_failed')
+                     ->will($this->returnValue(FALSE));
+
+        $this->result->expects($this->once())
+                     ->method('number_of_rows')
+                     ->will($this->returnValue(count($data)));
+
+        $this->result->expects($this->once())
+                     ->method('result_' . $format)
+                     ->will($this->returnValue($data));
+    }
+
+    /**
+     * Expect that a query returns no results.
+     *
+     * @param String $format Return result as 'array', 'row', 'column' or 'cell'
+     *
+     * @return void
+     */
+    public function expectNoResultsFound($format = 'array')
+    {
+        $mock = new FluidInterfaceMock();
+
+        $this->db->expects($this->once())
+                 ->method('get_new_dml_query_builder_object')
+                 ->will($this->returnValue($mock));
+
+        $this->db->expects($this->once())
+                 ->method('query')
+                 ->will($this->returnValue($this->result));
+
+        $this->result->expects($this->once())
+                     ->method('has_failed')
+                     ->will($this->returnValue(FALSE));
+
+        $this->result->expects($this->once())
+                     ->method('number_of_rows')
+                     ->will($this->returnValue(0));
+
+        $this->result->expects($this->never())
+                     ->method('result_' . $format);
+    }
+
+    /**
+     * Expect that a query returns an error.
+     *
+     * @return void
+     */
+    protected function expectQueryError()
+    {
+        $mock = new FluidInterfaceMock();
+
+        $this->db->expects($this->once())
+                 ->method('get_new_dml_query_builder_object')
+                 ->will($this->returnValue($mock));
+
+        $this->db->expects($this->once())
+                 ->method('query')
+                 ->will($this->returnValue($this->result));
+
+        $this->result->expects($this->once())
+                     ->method('has_failed')
+                     ->will($this->returnValue(TRUE));
+    }
+}
+
+?>
---
+++ src/Lunr/Halo/MySQLDatabaseAccessObjectTest.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * This file contains the MySQLDatabaseAccessObjectTest class.
+ *
+ * PHP Version 5.4
+ *
+ * @category   Libraries
+ * @package    Halo
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @copyright  2014, M2Mobi BV, Amsterdam, The Netherlands
+ * @license    http://lunr.nl/LICENSE MIT License
+ */
+
+namespace Lunr\Halo;
+
+use ReflectionClass;
+
+/**
+ * This class contains setup and tear down methods for DAOs using MySQL access.
+ *
+ * @category   Libraries
+ * @package    Halo
+ * @subpackage Libraries
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+abstract class MySQLDatabaseAccessObjectTest extends LunrBaseTest
+{
+
+    /**
+     * Mock instance of the MySQLConnection class.
+     * @var \Lunr\Gravity\Database\MySQL\MySQLConnection
+     */
+    protected $db;
+
+    /**
+     * Mock instance of the Logger class
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * Mock instance of the DMLQueryBuilder class
+     * @var \Lunr\Gravity\Database\MySQL\MySQLDMLQueryBuilder
+     */
+    protected $builder;
+
+    /**
+     * Mock instance of the QueryEscaper class
+     * @var \Lunr\Gravity\Database\MySQL\MySQLQueryEscaper
+     */
+    protected $escaper;
+
+    /**
+     * Mock instance of the QueryResult class
+     * @var \Lunr\Gravity\Database\MySQL\MySQLQueryResult
+     */
+    protected $result;
+
+    /**
+     * Testcase Constructor.
+     */
+    public function setUp()
+    {
+        $this->db = $this->getMockBuilder('Lunr\Gravity\Database\MySQL\MySQLConnection')
+                         ->disableOriginalConstructor()
+                         ->getMock();
+
+        $this->builder = $this->getMockBuilder('Lunr\Gravity\Database\MySQL\MySQLDMLQueryBuilder')
+                              ->disableOriginalConstructor()
+                              ->getMock();
+
+        $this->escaper = $this->getMockBuilder('Lunr\Gravity\Database\MySQL\MySQLQueryEscaper')
+                              ->disableOriginalConstructor()
+                              ->getMock();
+
+        $this->result = $this->getMockBuilder('Lunr\Gravity\Database\MySQL\MySQLQueryResult')
+                             ->disableOriginalConstructor()
+                             ->getMock();
+
+        $this->logger = $this->getMock('Psr\Log\LoggerInterface');
+
+        $this->db->expects($this->at(1))
+                 ->method('get_query_escaper_object')
+                 ->will($this->returnValue($this->escaper));
+
+        // Assumption: All DAO's end in DAO.
+        $name = str_replace('\\Tests\\', '\\', substr(static::class, 0, strrpos(static::class, 'DAO') + 3));
+
+        $this->class = new $name($this->db, $this->logger);
+
+        $this->reflection = new ReflectionClass($name);
+    }
+
+    /**
+     * Testcase Destructor.
+     */
+    public function tearDown()
+    {
+        unset($this->class);
+        unset($this->reflection);
+        unset($this->db);
+        unset($this->logger);
+        unset($this->builder);
+        unset($this->escaper);
+        unset($this->result);
+    }
+
+}
+
+?>
