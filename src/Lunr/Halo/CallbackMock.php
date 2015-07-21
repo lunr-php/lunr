a38c106c30c89afe728210002a010c5ba5d3f3b1
Heinz Wiesinger <pprkut@liwjatan.at>
2015-07-21 13:10:04 -0500

    Halo: Add CallbackMock class.
    This helps tests that test functions working with callback functions.

---
+++ src/Lunr/Halo/CallbackMock.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file contains the CallbackMock.
+ *
+ * PHP Version 5.4
+ *
+ * @package    Lunr\Halo
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ * @copyright  2015, M2Mobi BV, Amsterdam, The Netherlands
+ * @license    http://lunr.nl/LICENSE MIT License
+ */
+
+namespace Lunr\Halo;
+
+/**
+ * This mock class can be used when one want to mock callback functionality.
+ */
+class CallbackMock
+{
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+
+    }
+
+    /**
+     * A test function to be overridden by phpunit's mock funcitonality.
+     *
+     * @return void
+     */
+    public function test()
+    {
+
+    }
+
+}
+
+?>
