80112c03e1527c42a6b8f11a39c45a0e3a007be0
Damien Tardy-Panis <damien@m2mobi.com>
2016-05-17 15:30:40 +0200

    Vortex: add push notification multi dispatcher interface
    Add the missing unit tests declarations
    
    Reviewed at http://reviews.lunr.nl/r/438/

---
+++ src/Lunr/Vortex/PushNotificationMultiDispatcherInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file contains the PushNotificationMultiDispatcherInterface interface which
+ * is the base of all push notifications using a multicast sending.
+ *
+ * PHP Version 5.4
+ *
+ * @package    Lunr\Vortex
+ * @author     Damien Tardy-Panis <damien@m2mobi.com>
+ * @copyright  2016, M2Mobi BV, Amsterdam, The Netherlands
+ * @license    http://lunr.nl/LICENSE MIT License
+ */
+
+namespace Lunr\Vortex;
+
+/**
+ * Push notification multi interface.
+ */
+interface PushNotificationMultiDispatcherInterface extends PushNotificationDispatcherInterface
+{
+
+}
+
+?>
