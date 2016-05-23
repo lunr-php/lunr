299aba9554617beb4e0cee707b7ad79ec75b4fbf
Damien Tardy-Panis <damien@m2mobi.com>
2016-05-23 15:28:42 +0200

    Support: add ApnsPHP library setup
    Reviewed at http://reviews.lunr.nl/r/443/

--- support/setup.sh
+++ support/setup.sh
@@ -12,6 +12,7 @@ fi
 # interactions download locations
 PSRLOG="https://github.com/php-fig/log/archive/1.0.0.tar.gz"
 RESQUE="https://github.com/chrisboulton/php-resque/archive/1.2.tar.gz"
+APNSPHP="https://github.com/immobiliare/ApnsPHP/archive/v1.0.1.tar.gz"
 
 if ! [ -e "$DESTDIR/Psr/Log" ]; then
   cd "$TMP"
@@ -28,6 +29,15 @@ if ! [ -e "$DESTDIR/Resque" ]; then
     tar xvf php-resque-1.2.tar.gz
 
     mv php-resque-1.2/lib "$DESTDIR/Resque"
+  cd -
+fi
+
+if ! [ -e "$DESTDIR/ApnsPHP" ]; then
+  cd "$TMP"
+    wget --content-disposition "$APNSPHP"
+    tar xvf ApnsPHP-1.0.1.tar.gz
+
+    mv ApnsPHP-1.0.1/ApnsPHP "$DESTDIR/ApnsPHP"
   cd -
 fi
 
