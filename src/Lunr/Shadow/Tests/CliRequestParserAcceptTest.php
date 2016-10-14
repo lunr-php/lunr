7ae7c2a25e4ba4336b22c57b4ff6e7af9b325a57
Heinz Wiesinger <heinz@m2mobi.com>
2016-10-14 11:56:38 +0200

    Shadow: Fix undefined index warnings when parsing accept headers.

--- src/Lunr/Shadow/Tests/CliRequestParserAcceptTest.php
+++ src/Lunr/Shadow/Tests/CliRequestParserAcceptTest.php
@@ -87,6 +87,36 @@ class CliRequestParserAcceptTest extends CliRequestParserTest
     }
 
     /**
+     * Test that parse_accept_format() returns null when not specified.
+     *
+     * @requires extension http
+     * @requires function http\Header::negotiate
+     * @covers   Lunr\Shadow\CliRequestParser::parse_accept_format
+     */
+    public function testGetAcceptFormatReturnsNullWhenNotSpecified()
+    {
+        $this->assertNull($this->header->name);
+        $this->assertNull($this->header->value);
+
+        $property = $this->get_accessible_reflection_property('ast');
+        $ast      = $property->getValue($this->class);
+
+        unset($ast['accept-format']);
+
+        $property->setValue($this->class, $ast);
+
+        $this->header->expects($this->once())
+                     ->method('negotiate')
+                     ->with($this->equalTo([]))
+                     ->will($this->returnValue(NULL));
+
+        $this->assertNull($this->class->parse_accept_format([]));
+
+        $this->assertSame('Accept', $this->header->name);
+        $this->assertNull($this->header->value);
+    }
+
+    /**
      * Test that parse_accept_language() returns content type when called with a valid set of supported languages.
      *
      * @param String $value the expected value
@@ -150,6 +180,36 @@ class CliRequestParserAcceptTest extends CliRequestParserTest
     }
 
     /**
+     * Test that parse_accept_format() returns null when not specified.
+     *
+     * @requires extension http
+     * @requires function http\Header::negotiate
+     * @covers   Lunr\Shadow\CliRequestParser::parse_accept_language
+     */
+    public function testGetAcceptLanguageReturnsNullWhenNotSpecified()
+    {
+        $this->assertNull($this->header->name);
+        $this->assertNull($this->header->value);
+
+        $property = $this->get_accessible_reflection_property('ast');
+        $ast      = $property->getValue($this->class);
+
+        unset($ast['accept-language']);
+
+        $property->setValue($this->class, $ast);
+
+        $this->header->expects($this->once())
+                     ->method('negotiate')
+                     ->with($this->equalTo([]))
+                     ->will($this->returnValue(NULL));
+
+        $this->assertNull($this->class->parse_accept_language([]));
+
+        $this->assertSame('Accept-Language', $this->header->name);
+        $this->assertNull($this->header->value);
+    }
+
+    /**
      * Test that parse_accept_charset() returns content type when called with a valid set of supported charsets.
      *
      * @param String $value the expected value
@@ -210,6 +270,36 @@ class CliRequestParserAcceptTest extends CliRequestParserTest
 
         $this->assertSame('Accept-Charset', $this->header->name);
         $this->assertSame('accept_charset_value', $this->header->value);
+    }
+
+    /**
+     * Test that parse_accept_charset() returns null when not specified.
+     *
+     * @requires extension http
+     * @requires function http\Header::negotiate
+     * @covers   Lunr\Shadow\CliRequestParser::parse_accept_charset
+     */
+    public function testGetAcceptCharsetReturnsNullWhenNotSpecified()
+    {
+        $this->assertNull($this->header->name);
+        $this->assertNull($this->header->value);
+
+        $property = $this->get_accessible_reflection_property('ast');
+        $ast      = $property->getValue($this->class);
+
+        unset($ast['accept-charset']);
+
+        $property->setValue($this->class, $ast);
+
+        $this->header->expects($this->once())
+                     ->method('negotiate')
+                     ->with($this->equalTo([]))
+                     ->will($this->returnValue(NULL));
+
+        $this->assertNull($this->class->parse_accept_charset([]));
+
+        $this->assertSame('Accept-Charset', $this->header->name);
+        $this->assertNull($this->header->value);
     }
 
 }
