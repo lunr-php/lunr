581c1d0a6d4b43da2586901d55c833877014709a
Heinz Wiesinger <pprkut@liwjatan.at>
2018-07-25 10:10:07 +0200

    Corona: Add a Bad Gateway Exception class.
    Reviewed at https://reviews.lunr.nl/r/573/

---
+++ src/Lunr/Corona/Exceptions/BadGatewayException.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file contains the BadGatewayException class.
+ *
+ * PHP Version 7.0
+ *
+ * @package   Lunr\Corona\Exceptions
+ * @author    Heinz Wiesinger <heinz@m2mobi.com>
+ * @copyright 2018, M2Mobi BV, Amsterdam, The Netherlands
+ * @license   http://lunr.nl/LICENSE MIT License
+ */
+
+namespace Lunr\Corona\Exceptions;
+
+use \Lunr\Corona\HttpCode;
+use \Exception;
+
+/**
+ * Exception for the BadGateway HTTP error (502).
+ */
+class BadGatewayException extends HttpException
+{
+
+    /**
+     * Constructor.
+     */
+    public function __construct($message = NULL, $app_code = 0, Exception $previous = NULL)
+    {
+        parent::__construct($message, HttpCode::BAD_GATEWAY, $app_code, $previous);
+    }
+
+}
+
+?>
---
+++ src/Lunr/Corona/Exceptions/Tests/BadGatewayExceptionBaseTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * This file contains the BadGatewayExceptionBaseTest class.
+ *
+ * PHP Version 7.0
+ *
+ * @package Lunr\Corona\Exceptions
+ * @author  Heinz Wiesinger <heinz@m2mobi.com>
+ */
+
+namespace Lunr\Corona\Exceptions\Tests;
+
+use Lunr\Corona\Exceptions\Tests\Helpers\HttpExceptionTest;
+use Exception;
+
+/**
+ * This class contains tests for the BadGatewayException class.
+ *
+ * @covers Lunr\Corona\Exceptions\BadGatewayException
+ */
+class BadGatewayExceptionBaseTest extends HttpExceptionTest
+{
+
+    /**
+     * Test that the error code was set correctly.
+     */
+    public function testErrorCodeSetCorrectly()
+    {
+        $this->assertPropertySame('code', 502);
+    }
+
+    /**
+     * Test that the error code was set correctly.
+     */
+    public function testApplicationErrorCodeSetCorrectly()
+    {
+        $this->assertPropertySame('app_code', $this->code);
+    }
+
+    /**
+     * Test that the error message was passed correctly.
+     */
+    public function testErrorMessagePassedCorrectly()
+    {
+        $this->expectExceptionMessage($this->message);
+
+        throw $this->class;
+    }
+
+}
+
+?>
