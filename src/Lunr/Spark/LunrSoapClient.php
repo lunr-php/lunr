63de58357d618057e369952b7d5eda3b96810e3e
Heinz Wiesinger <heinz@m2mobi.com>
2023-03-19 19:45:03 +0100

    Spark: Switch to SPDX license and copyright headers
    Reviewed at https://reviews.lunr.nl/r/937/

--- src/Lunr/Spark/LunrSoapClient.php
+++ src/Lunr/Spark/LunrSoapClient.php
@@ -3,10 +3,9 @@
 /**
  * This file contains the LunrSoapClient class.
  *
- * @package    Lunr\Spark
- * @author     Damien Tardy-Panis <damien@m2mobi.com>
- * @copyright  2013-2018, M2Mobi BV, Amsterdam, The Netherlands
- * @license    http://lunr.nl/LICENSE MIT License
+ * SPDX-FileCopyrightText: Copyright 2013 M2mobi B.V., Amsterdam, The Netherlands
+ * SPDX-FileCopyrightText: Copyright 2022 Move Agency Group B.V., Zwolle, The Netherlands
+ * SPDX-License-Identifier: MIT
  */
 
 namespace Lunr\Spark;
