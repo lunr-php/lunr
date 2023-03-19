4b4488ecfff273dbfcd2ca0d3536eb4b81bef466
Heinz Wiesinger <heinz@m2mobi.com>
2023-03-19 17:54:46 +0100

    General: Remove no longer needed test statics
    Reviewed at https://reviews.lunr.nl/r/930/

--- tests/statics/Core/PSR-4/PSR4TestFile.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * This file contains the PSR4TestFile.php
- *
- * So basically it's empty
- *
- * @package Tests\statics
- * @author  Sean Molenaar<sean@seanmolenaar.eu>
- */
--- tests/statics/Core/locator/locate.recipeidnotarray.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$recipe = [];
-
-$recipe['recipeidnotarray'] = 'string';
-
-?>
