c1107e1b77e900eeed6a40cc5861a941721d55e5
Brian Stoop <brian.stoop@moveagency.com>
2024-11-08 11:46:20 +0100

    Corona: Add http exception for temporarily disabled API's
    Reviewed at https://reviews.lunr.nl/r/1227/

---
+++ src/Lunr/Corona/Exceptions/Tests/TemporarilyDisabledExceptionBaseTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file contains the TemporarilyDisabledExceptionBaseTest class.
+ *
+ * SPDX-FileCopyrightText: Copyright 2024 Move Agency Group B.V., Zwolle, The Netherlands
+ * SPDX-License-Identifier: MIT
+ */
+
+namespace Lunr\Corona\Exceptions\Tests;
+
+/**
+ * This class contains tests for the TemporarilyDisabledException class.
+ *
+ * @covers Lunr\Corona\Exceptions\TemporarilyDisabledException
+ */
+class TemporarilyDisabledExceptionBaseTest extends TemporarilyDisabledExceptionTest
+{
+
+    /**
+     * Test that the error code was set correctly.
+     */
+    public function testErrorCodeSetCorrectly(): void
+    {
+        $this->assertPropertySame('code', 540);
+    }
+
+    /**
+     * Test that the error code was set correctly.
+     */
+    public function testApplicationErrorCodeSetCorrectly(): void
+    {
+        $this->assertPropertySame('app_code', $this->code);
+    }
+
+    /**
+     * Test that the error message was passed correctly.
+     */
+    public function testErrorMessagePassedCorrectly(): void
+    {
+        $this->expectExceptionMessage($this->message);
+
+        throw $this->class;
+    }
+
+}
+
+?>
