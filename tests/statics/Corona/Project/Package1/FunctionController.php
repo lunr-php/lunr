a0d9ac3134a6d912f393e2fc9c3acae4dda253b1
Heinz Wiesinger <heinz.wiesinger@moveagency.com>
2025-07-11 11:12:29 +0200

    Corona: Fix codestyle in test statics

--- tests/statics/Corona/Project/Package1/FunctionController.php
+++ tests/statics/Corona/Project/Package1/FunctionController.php
@@ -1,1 +1,10 @@
+<?php
 
+/**
+ * Lunr.Corona test static.
+ * SPDX-FileCopyrightText: Copyright 2013 M2mobi B.V., Amsterdam, The Netherlands
+ * SPDX-FileCopyrightText: Copyright 2022 Move Agency Group B.V., Zwolle, The Netherlands
+ * SPDX-License-Identifier: MIT
+ */
+
+?>
